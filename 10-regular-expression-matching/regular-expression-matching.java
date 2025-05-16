0b71607a738797e53a8850f6136082a3629cd07b
MNikhilBharath <67653205+MNikhilBharath@users.noreply.github.com>
2025-05-16 10:28:18 -0400

    Time: 572 ms (10.39%) | Memory: 42 MB (87.02%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,23 +1,30 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        return helper(s, p, 0, 0);
+        boolean[][] dp = new boolean[s.length()+1][p.length()+1];
+        return helper(s, p, 0, 0, dp);
     }
-    public boolean helper(String s, String p, int i, int j){
+    public boolean helper(String s, String p, int i, int j, boolean[][] dp){
         int n1=s.length();
         int n2=p.length();
-        if(i>=n1 && j>=n2){
+        // if(i>n1 || j>n2){
+
+        // }
+        if(i<n1 && j<n2 && dp[i][j]==true){
             return true;
         }
+        if(i>=n1 && j>=n2){
+            return dp[i][j]=true;
+        }
         if(j>=n2){
-            return false;
+            return dp[i][j]=false;
         }
         boolean match = i<n1 && (s.charAt(i)==p.charAt(j) || p.charAt(j)=='.');
         if((j+1)<n2 && p.charAt(j+1)=='*'){
-            return helper(s,p,i,j+2) || (match && helper(s,p,i+1, j));
+            return dp[i][j]=helper(s,p,i,j+2, dp) || (match && helper(s,p,i+1, j, dp));
         }
         if(match){
-            return helper(s,p,i+1,j+1);
+            return dp[i][j]=helper(s,p,i+1,j+1, dp);
         }
-        return false;
+        return dp[i][j]=false;
     }
 }
