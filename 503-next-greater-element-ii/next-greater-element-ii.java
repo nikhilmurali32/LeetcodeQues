2637e83e4af54b924372f71bd7a65b5f9f768d66
MNikhilBharath <67653205+MNikhilBharath@users.noreply.github.com>
2025-05-24 16:00:54 -0400

    Time: 15 ms (66.79%) | Memory: 46.3 MB (50.69%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,34 +1,15 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
+        int n=nums.length;
+        int[] res = new int[n];
+        Arrays.fill(res, -1);
         Stack<Integer> st = new Stack<>();
-        HashMap<Integer, Integer> hmap = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            while(!st.isEmpty() && nums[st.peek()]<nums[i]){
-                hmap.put(st.pop(), nums[i]);
+        for(int i=0; i<n*2; i++){
+            while(!st.isEmpty() && nums[st.peek()]<nums[i%n]){
+                res[st.pop()]=nums[i%n];
             }
-            st.push(i);
+            st.push(i%n);
         }
-        int n=nums.length-1;
-        while(n>=0 && st.size()>1){
-            for(int num: nums){
-                while(st.size()>1 && num>nums[st.peek()]){
-                    hmap.put(st.pop(), num);
-                }
-                if(st.size()==1){
-                    break;
-                }
-            }
-            if(st.size()==1){
-                break;
-            }
-            n--;
-        }
-        while(!st.isEmpty()){
-            hmap.put(st.pop(), -1);
-        }
-        for(int i=0; i<nums.length; i++){
-            nums[i]=hmap.get(i);
-        }
-        return nums;
+        return res;
     }
 }
