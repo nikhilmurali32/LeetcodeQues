bb9ed2f7bc407a0c1628f366d1dc5072fc5ce769
MNikhilBharath <67653205+MNikhilBharath@users.noreply.github.com>
2025-05-26 13:37:56 -0400

    Time: 7 ms (100.00%) | Memory: 49.4 MB (74.44%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,26 +1,41 @@
 class NumArray {
-    int[] nums;
-    public NumArray(int[] nums) {
-        // for(int i)
-        this.nums=nums;
-    }
+    // int[] _nums;
+    // public NumArray(int[] nums) {
+    //     // for(int i)
+    //     this._nums=nums;
+    // }
     
-    public int sumRange(int left, int right) {
-        int sum_left=0;
-        int sum_right=0;
-        for(int i=0; i<nums.length; i++){
-            if(i<left){
-                sum_left += nums[i];
-            }
-            if(i<=right){
-                sum_right += nums[i];
-                if(i==right){
-                    break;
-                }
-            }
-        }
-        return sum_right-sum_left;
-    }
+    // public int sumRange(int left, int right) {
+    //     int sum_left=0;
+    //     int sum_right=0;
+    //     for(int i=0; i<_nums.length; i++){
+    //         if(i<left){
+    //             sum_left += _nums[i];
+    //         }
+    //         if(i<=right){
+    //             sum_right += _nums[i];
+    //             if(i==right){
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     return sum_right-sum_left;
+    // }
+int[] nums;
+
+public NumArray(int[] nums) {
+    for(int i = 1; i < nums.length; i++)
+        nums[i] += nums[i - 1];
+    
+    this.nums = nums;
+}
+
+public int sumRange(int i, int j) {
+    if(i == 0)
+        return nums[j];
+    
+    return nums[j] - nums[i - 1];
+}
 }
 
 /**
