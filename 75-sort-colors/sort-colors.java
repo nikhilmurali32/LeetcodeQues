fd85ff7a2db3bd5cbc14c7dffa7857dbc60d0f32
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2025-11-03 10:15:50 -0500

    Time: 0 ms (100.00%) | Memory: 42.9 MB (6.19%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,33 +1,28 @@
 class Solution {
     public void sortColors(int[] nums) {
-        HashMap<Integer, Integer> hmap = new HashMap<>();
-        for(int num:nums){
-            hmap.put(num, hmap.getOrDefault(num, 0)+1);
-        }
-        int num_zeros = 0;
-        int num_ones = 0;
-        int num_twos = 0;
-        if(hmap.containsKey(0)){
-            num_zeros = hmap.get(0);
-        }
-        if(hmap.containsKey(1)){
-            num_ones = hmap.get(1);
-        }
-        if(hmap.containsKey(2)){
-            num_twos = hmap.get(2);
-        }
+        int l=0;
         int i=0;
-        while(i<num_zeros){
-            nums[i]=0;
-            i++;
+        int r=nums.length-1;
+        while(i<nums.length && i<=r){
+            if(nums[i]==0){
+                swap(i, l, nums);
+                // i++;
+                l++;
+                i++;
+            }
+            else if(nums[i]==1){
+                i++;
+            }
+            else{
+                swap(i, r, nums);
+                r--;
+                // i++;
+            }
         }
-        while(i<num_ones+num_zeros){
-            nums[i]=1;
-            i++;
-        }
-        while(i<num_twos+num_ones+num_zeros){
-            nums[i]=2;
-            i++;
-        }
+    }
+    public void swap(int i, int j, int[] nums){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
 }
