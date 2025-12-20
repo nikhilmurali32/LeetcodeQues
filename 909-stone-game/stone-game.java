ff4d5a5720fcb9f8d4cbe24de6bf4687e7d94eaf
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2025-12-20 13:44:24 -0500

    Time: 48 ms (5.19%) | Memory: 74.2 MB (5.01%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,41 +1,33 @@
 class Solution {
-    int alice=0;
-    int bob=0;
+    int[][][] dp;
+    int max=0;
     public boolean stoneGame(int[] piles) {
-        return true;
-        // int sum=0;
-        // int n=piles.length;
-        // for(int i=0; i<n; i++){
-        //     sum += piles[i];
-        // }
-        // helper(piles, 'a', 0, n-1);
-        // return alice>bob?true:false;
+        int n=piles.length;
+        int sum=0;
+        for(int pile:piles){
+            sum+=pile;
+        }
+        dp = new int[n+1][n+1][2];
+        for(int[][] arr: dp){
+            for(int[] subArr: arr){
+                Arrays.fill(subArr, -1);
+            }
+        }
+        return helper(piles, 1, 0, n-1)>=0;
     }
-    public void helper(int[] piles, char c, int l, int r){
-        if(l>r){
-            return;
+    public int helper(int[] piles, int id, int l, int r){
+        if(r<l){
+            return 0;
         }
-        if(c=='a'){
-            if(piles[l]>piles[r]){
-                alice += piles[l];
-                l++;
-            }
-            else{
-                alice += piles[r];
-                r--;
-            }
-            helper(piles, 'b', l, r);
+        if(dp[l][r][id]!=-1){
+            return dp[l][r][id];
         }
-        else{
-            if(piles[l]>=piles[r]){
-                bob += piles[l];
-                l++;
-            }
-            else{
-                bob += piles[r];
-                r--;
-            }
-            helper(piles, 'a', l, r);
+        if(id==1){
+            dp[l][r][1] = Math.max(piles[l]+helper(piles, 0, l+1, r), piles[r]+helper(piles, 0, l, r-1));
         }
+        if(id==0){
+            dp[l][r][0] = Math.max(-piles[l]+helper(piles, 1, l+1, r), -piles[r]+helper(piles, 1, l, r-1));
+        }
+        return dp[l][r][id];
     }
 }
