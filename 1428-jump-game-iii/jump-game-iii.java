3fa8278d34cb0243924d97eb9cc221a3331f0254
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-01-01 14:29:11 -0500

    Time: 2 ms (83.33%) | Memory: 52.9 MB (42.20%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -0,0 +1,28 @@
+class Solution {
+    boolean done=false;
+    public boolean canReach(int[] arr, int start) {
+        boolean[] vis=new boolean[arr.length];
+        return helper(arr, start, vis);
+    }
+    public boolean helper(int[] arr, int start, boolean[] vis){
+        // if(done){
+        //     return true;
+        // }
+        if(start<0 || start>=arr.length || vis[start]){
+            return false;
+        }
+        if(arr[start]==0){
+            return true;
+        }
+        vis[start]=true;
+        if(helper(arr, start+arr[start], vis)){
+            // done=true;
+            return true;
+        }
+        if(helper(arr, start-arr[start], vis)){
+            done=true;
+            return true;
+        }
+        return false;
+    }
+}
