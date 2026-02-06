8d2c3a591451f047bef5726afd56d37954a16a1f
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-02-06 16:45:28 -0500

    Time: 11 ms (51.14%) | Memory: 46.7 MB (91.10%) - LeetSync

--- 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.java
+++ 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.java
@@ -15,17 +15,17 @@ class Solution {
         // if(s.charAt(n-1)=='0'){
         //     zeros[n-1]=1;
         // }
-        for(int i=n-1; i>=0; i--){
-            if(s.charAt(i)=='0'){
-                zeros[i] = zeros[i+1]+1;
-            }
-            else{
-                zeros[i]=zeros[i+1];
-            }
-        }
+        // for(int i=n-1; i>=0; i--){
+        //     if(s.charAt(i)=='0'){
+        //         zeros[i] = zeros[i+1]+1;
+        //     }
+        //     else{
+        //         zeros[i]=zeros[i+1];
+        //     }
+        // }
         int res=n;
         for(int i=0; i<=n; i++){
-            res = Math.min(res, ones_before[i]+zeros[i]);
+            res = Math.min(res, ones_before[i]+((n-i)-(ones_before[n]-ones_before[i])));
         }
         return res;
     }
