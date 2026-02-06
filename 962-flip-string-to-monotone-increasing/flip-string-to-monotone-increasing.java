ea07cccbe7db721833bd0ad6e0325c3ddf352c22
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-02-06 16:14:08 -0500

    Time: 15 ms (32.42%) | Memory: 47.1 MB (40.64%) - LeetSync

--- 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.java
+++ 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.java
@@ -1,10 +1,10 @@
 class Solution {
     public int minFlipsMonoIncr(String s) {
         int n=s.length();
-        int[] ones_before = new int[n];
-        int[] zeros_after = new int[n];
+        int[] ones_before = new int[n+1];
+        int[] zeros = new int[n+1];
 
-        for(int i=1; i<n; i++){
+        for(int i=1; i<=n; i++){
             if(s.charAt(i-1)=='1'){
                 ones_before[i] = ones_before[i-1]+1;
             }
@@ -12,17 +12,20 @@ class Solution {
                 ones_before[i]=ones_before[i-1];
             }
         }
-        for(int i=n-2; i>=0; i--){
-            if(s.charAt(i+1)=='0'){
-                zeros_after[i] = zeros_after[i+1]+1;
+        // if(s.charAt(n-1)=='0'){
+        //     zeros[n-1]=1;
+        // }
+        for(int i=n-1; i>=0; i--){
+            if(s.charAt(i)=='0'){
+                zeros[i] = zeros[i+1]+1;
             }
             else{
-                zeros_after[i]=zeros_after[i+1];
+                zeros[i]=zeros[i+1];
             }
         }
         int res=n;
-        for(int i=0; i<n; i++){
-            res = Math.min(res, ones_before[i]+zeros_after[i]);
+        for(int i=0; i<=n; i++){
+            res = Math.min(res, ones_before[i]+zeros[i]);
         }
         return res;
     }
