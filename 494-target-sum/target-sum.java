550774544ac1aa6dd3ac56bf5384dfe880c08d17
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-02-06 22:14:10 -0500

    Time: 119 ms (34.68%) | Memory: 47.4 MB (6.14%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,9 +1,10 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return helper(nums, 0, 0, target, nums.length);
+        HashMap<String, Integer> hmap = new HashMap<>();
+        return helper(nums, 0, 0, target, nums.length, hmap);
 
     }
-    public int helper(int[] nums, int sum, int ind, int target, int n){
+    public int helper(int[] nums, int sum, int ind, int target, int n, HashMap<String, Integer> hmap){
         if(ind==n){
             if(sum==target){
                 return 1;
@@ -12,9 +13,14 @@ class Solution {
                 return 0;
             }
         }
-        int add = helper(nums, sum+nums[ind], ind+1, target, n);
-        int sub = helper(nums, sum-nums[ind], ind+1, target, n);
+        String key = ind + "," + sum;
+        if(hmap.containsKey(key)){
+            return hmap.get(key);
+        }
+        int add = helper(nums, sum+nums[ind], ind+1, target, n, hmap);
+        int sub = helper(nums, sum-nums[ind], ind+1, target, n, hmap);
         int ways = add+sub;
+        hmap.put(key, ways);
         return ways;
     }
 }
