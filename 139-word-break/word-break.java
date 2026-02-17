160de65d12de562816109e4a2d62c9d497797065
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-02-16 23:57:22 -0500

    Time: 6 ms (62.30%) | Memory: 46.4 MB (31.84%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,39 +1,23 @@
 class Solution {
+    Boolean[] dp;
     public boolean wordBreak(String s, List<String> wordDict) {
-        int n=s.length();
-        int i=0;
-        boolean[] dp = new boolean[n+1];
-        dp[0]=false;
-        String str="";
-        while(i<n){
-            str += s.charAt(i);
-            if(wordDict.contains(str)){
-                dp[i+1]=true;
-            }
-            else{
-                int j=i;
-                String temp = "";
-                int seen=0;
-                while(j>=0){
-                    temp = s.charAt(j)+temp;
-                    if(wordDict.contains(temp) && dp[j]){
-                        // dp[i]=true;
-                        seen=1;
-                        break;
-                    }
-                    j--;
-                }
-                if(seen==1){
-                    dp[i+1]=true;
-                }
-                else{
-                    dp[i+1]=false;
-                }
-            }
-            System.out.println(str);
-            System.out.println(dp[i]);
-            i++;            
+        dp = new Boolean[s.length()];
+        Set<String> hset = new HashSet<>(wordDict);
+        return helper(s, hset, 0);
+    }
+    public boolean helper(String s, Set<String> hset, int ind){
+        if(ind==s.length()){
+            return true;
         }
-        return dp[n];
+        if(dp[ind]!=null){
+            return dp[ind];
+        }
+        for(int i=ind+1; i<=s.length(); i++){
+            String str = s.substring(ind, i);
+            if(hset.contains(str) && helper(s, hset, i)){
+                return dp[ind]=true;
+            }
+        }
+        return dp[ind]=false;
     }
 }
