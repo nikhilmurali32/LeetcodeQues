db954389a748320424ca9600e933df99cf600945
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-02-21 13:26:09 -0500

    Time: 3 ms (91.24%) | Memory: 44.6 MB (67.33%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -6,22 +6,16 @@ class Solution {
         if(s.length()==0){
             return true;
         }
-        HashSet<Character> hset = new HashSet<>();
         int[] freq = new int[26];
         for(char ch:s.toCharArray()){
-            hset.add(ch);
             freq[ch-'a']++;
         }
-        int count=0;
         for(char ch:t.toCharArray()){
             if(freq[ch-'a']==0){
                 return false;
             }
             freq[ch-'a']--;
-            if(freq[ch-'a']==0){
-                count++;
-            }
         }
-        return count==hset.size()?true:false;
+        return true;
     }
 }
