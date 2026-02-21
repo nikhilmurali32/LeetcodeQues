bbfb32a0291f7a842adff64302413068693c0370
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-02-20 22:49:35 -0500

    Time: 10 ms (62.63%) | Memory: 105.7 MB (26.26%) - LeetSync

--- 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.java
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.java
@@ -16,69 +16,40 @@
 class Solution {
     int count=0;
     public int pseudoPalindromicPaths (TreeNode root) {
-        helper(root, new ArrayList<>(), new HashMap<>(), 0);
+        helper(root, new ArrayList<>(), new int[10], 0);
         return count;
     }
-    public void helper(TreeNode root, List<Integer> list, HashMap<Integer, Integer> hmap, int countOdd){
+    public void helper(TreeNode root, List<Integer> list, int[] freq, int countOdd){
         if(root==null){
             return;
         }
-        list.add(root.val);
-        if(hmap.isEmpty() || !hmap.containsKey(root.val)){
+        // list.add(root.val);
+        if(freq[root.val]==0){
             countOdd++;
         }
-        else if(hmap.get(root.val)%2==0){
+        else if(freq[root.val]%2==0){
             countOdd++;
         }
-        else if(hmap.get(root.val)%2!=0){
+        else if(freq[root.val]%2!=0){
             countOdd--;
         }
-        hmap.put(root.val, hmap.getOrDefault(root.val, 0)+1);
+        freq[root.val]++;
         if(root.left!=null){
-            helper(root.left, list, hmap, countOdd);
+            helper(root.left, list, freq, countOdd);
         }
         if(root.right!=null){
-            helper(root.right, list, hmap, countOdd);
+            helper(root.right, list, freq, countOdd);
         }
         if(root.left==null && root.right==null){
             if(countOdd<=1){
                 count++;
             }
         }
-        int lastEle = list.get(list.size()-1);
-        hmap.put(lastEle, hmap.get(lastEle)-1);
-        if(hmap.get(lastEle)%2 !=0){
+        // int lastEle = list.get(list.size()-1);
+        freq[root.val]--;
+        if(freq[root.val]%2 !=0){
             countOdd--;
         }
-        list.remove(list.size()-1);
-    }
-    public boolean check(List<Integer> list){
-        int[] arr = new int[10];
-        int size=list.size();
-        for(int i=0; i<list.size(); i++){
-            arr[list.get(i)]++;
-        }
-        int odd_count=0, even_count=0;
-        for(int i=1; i<10; i++){
-            if(arr[i]==0){
-                continue;
-            }
-            if(arr[i]%2==0){
-                even_count++;
-            }
-            else{
-                odd_count++;
-            }
-        }
-        if(odd_count>1){
-            return false;
-        }
-        if(odd_count==0){
-            return true;
-        }
-        if(odd_count==1 && (size%2 != 0)){
-            return true;
-        }
-        return false;
+        // list.remove(list.size()-1);
     }
 }
