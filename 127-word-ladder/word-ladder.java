87ca2b290d5700333a2069e5cd180916ae0dbbd4
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-02-21 12:49:19 -0500

    Time: 379 ms (15.22%) | Memory: 57.5 MB (6.96%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,45 +1,70 @@
 class Solution {
+    HashMap<String, List<String>> hmap;
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> hset = new HashSet<>(wordList);
-        if(!hset.contains(endWord)){
-            return 0;
+        hmap = new HashMap<>();
+        Set<String> wordSet = new HashSet<>(wordList);
+        adjList(beginWord, wordSet);
+        for(String word:wordList){
+            adjList(word, wordSet);
         }
+        HashSet<String> hset = new HashSet<>();
         Queue<String> q = new LinkedList<>();
-        q.add(beginWord);
+        q.offer(beginWord);
         int steps=1;
+        int seen=0;
         while(!q.isEmpty()){
             int size=q.size();
             for(int i=0; i<size; i++){
-                String word = q.poll();
+                String word=q.poll();
+                // System.out.println(word);
                 if(word.equals(endWord)){
+                    seen=1;
                     return steps;
                 }
-                List<String> list = getNeighbours(word, hset);
-                for(String str:list){
-                    q.add(str);
+                if(hset.contains(word)){
+                    continue;
                 }
+                hset.add(word);
+                if(!hmap.containsKey(word)){
+                    continue;
+                }
+                for(String str:hmap.get(word)){
+                    System.out.println(str);
+                    q.offer(str);
+                }
+                System.out.println(" ");
             }
             steps++;
         }
-        return 0;
+        return seen==0?0:steps;        
+
     }
-    public List<String> getNeighbours(String word, Set<String> hset){
-        List<String> res = new ArrayList<>();
-        char[] str_arr = word.toCharArray();
-        for(int i=0; i<word.length(); i++){
-            char org = str_arr[i];
+    public void adjList(String word, Set<String> wordSet){
+        char[] ch_arr = word.toCharArray();
+        for(int i=0; i<ch_arr.length; i++){
+            char org=ch_arr[i];
             for(char c='a'; c<='z'; c++){
                 if(c==org){
                     continue;
                 }
-                str_arr[i]=c;
-                String new_word = new String(str_arr);
-                if(hset.remove(new_word)){
-                    res.add(new_word);
+                ch_arr[i]=c;
+                String str=new String(ch_arr);
+                if(wordSet.contains(str)){
+                    // System.out.println(str);
+                    if(!hmap.containsKey(word)){
+                        List<String> l = new ArrayList<>();
+                        l.add(str);
+                        hmap.put(word, l);
+                    }
+                    else{
+                        List<String> l = hmap.get(word);
+                        l.add(str);
+                        hmap.put(word, l);
+                    }
                 }
             }
-            str_arr[i]=org;
+            ch_arr[i]=org;
         }
-        return res;
+        // System.out.println(hmap.get(word).size());
     }
 }
