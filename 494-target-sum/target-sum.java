35a8ed6b0570922049059e7827a683f5690eef97
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-02-22 11:56:44 -0500

    Time: 102 ms (40.40%) | Memory: 47.4 MB (8.31%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,26 +1,21 @@
 class Solution {
+    HashMap<String, Integer> hmap;
     public int findTargetSumWays(int[] nums, int target) {
-        HashMap<String, Integer> hmap = new HashMap<>();
-        return helper(nums, 0, 0, target, nums.length, hmap);
-
+        hmap = new HashMap<>();
+        return helper(nums, target, 0, 0);
     }
-    public int helper(int[] nums, int sum, int ind, int target, int n, HashMap<String, Integer> hmap){
-        if(ind==n){
-            if(sum==target){
-                return 1;
-            }
-            else{
-                return 0;
-            }
+    public int helper(int[] nums, int target, int sum, int ind){
+        if(ind==nums.length){
+            return sum==target?1:0;
         }
-        String key = ind + "," + sum;
-        if(hmap.containsKey(key)){
-            return hmap.get(key);
+        String str = ind + "#" + sum;
+        if(hmap.containsKey(str)){
+            return hmap.get(str);
         }
-        int add = helper(nums, sum+nums[ind], ind+1, target, n, hmap);
-        int sub = helper(nums, sum-nums[ind], ind+1, target, n, hmap);
-        int ways = add+sub;
-        hmap.put(key, ways);
-        return ways;
+        int total=0;
+        total += helper(nums, target, sum+nums[ind], ind+1);
+        total += helper(nums, target, sum-nums[ind], ind+1);
+        hmap.put(str, total);
+        return total;
     }
 }
