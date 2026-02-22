d07345546fb5c9ab33c0aa3ad5bcd628924e7c1c
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-02-22 16:57:24 -0500

    Time: 4 ms (80.57%) | Memory: 46.9 MB (45.06%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -8,35 +8,35 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Node{
-    ListNode node;
-    int val;
-    Node(ListNode node, int val){
-        this.node=node;
-        this.val=val;
-    }
-}
+// class Node{
+//     ListNode node;
+//     int val;
+//     Node(ListNode node, int val){
+//         this.node=node;
+//         this.val=val;
+//     }
+// }
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> (a.val-b.val));
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> (a.val-b.val));
         for(ListNode node:lists){
-            ListNode tempN=node;
-            while(tempN != null){
-                pq.add(new Node(tempN, tempN.val));
-                tempN=tempN.next;
+            if(node!=null){
+                pq.add(node);
             }
         }
         if(pq.isEmpty()){
             return null;
         }
-        ListNode tempNode = pq.poll().node;
+        ListNode tempNode = new ListNode(0);;
         ListNode res=tempNode;
         while(!pq.isEmpty()){
-            Node temp_node = pq.poll();
-            tempNode.next = temp_node.node;
-            temp_node.node.next=null;
+            ListNode temp_node = pq.poll();
+            tempNode.next = temp_node;
+            if(temp_node.next!=null){
+                pq.add(temp_node.next);
+            }
             tempNode=tempNode.next;
         }
-        return res;
+        return res.next;
     }
 }
