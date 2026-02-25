56aa1789d9650419b8017fdc3e5b3c76e0d98cbe
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-02-25 13:24:44 -0500

    Time: 0 ms (100.00%) | Memory: 43.1 MB (23.44%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.java
+++ 59-spiral-matrix-ii/spiral-matrix-ii.java
@@ -2,43 +2,27 @@ class Solution {
     int[][] res;
     char ch=' ';
     int val;
-    List<Integer> list;
+    int len;
     public int[][] generateMatrix(int n) {
-        int[][] matrix = new int[n][n];
-        list = new ArrayList<>();
+        len=n;
         res = new int[n][n];
-        int num=1;
         val=1;
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                matrix[i][j] = num;
-                num++;
-            }
-        }
-        for(int i=0; i<n; i++){
             ch='r';
-            dfs(matrix, i, i, n-i);
+            dfs(i, i, n-i);
             ch=' ';
         }
-        // while(!list.isEmpty()){
-        //     System.out.println(list.remove(0));
-        // }
-        // for(int i=0; i<n; i++){
-        //     for(int j=0; j<n; j++){
-        //         res[i][j]=list.remove(0);
-        //     }
-        // }
         return res;
     }
-    public void dfs(int[][] matrix, int i, int j, int n){
-        if(i<matrix.length-n || i>n-1 || j<matrix[0].length-n || j>n-1 || res[i][j] != 0){
+    public void dfs( int i, int j, int n){
+        if(i<len-n || i>n-1 || j<len-n || j>n-1 || res[i][j] != 0){
             if(j>n-1){
                 ch='d';
             }
             else if(i>n-1){
                 ch='l';
             }
-            else if(j<matrix[0].length-n){
+            else if(j<len-n){
                 ch='u';
             }
             else{
@@ -46,23 +30,19 @@ class Solution {
             }
             return;
         }
-        System.out.println(i);
-        System.out.println(j);
-        System.out.println(" ");
-        list.add(matrix[i][j]);
         res[i][j]=val;
         val++;
         if(ch=='r'){
-            dfs(matrix, i, j+1, n);
+            dfs(i, j+1, n);
         }
         if(ch=='d'){
-            dfs(matrix, i+1, j, n);
+            dfs(i+1, j, n);
         }
         if(ch=='l'){
-            dfs(matrix, i, j-1, n);
+            dfs(i, j-1, n);
         }
         if(ch=='u'){
-            dfs(matrix, i-1, j, n);
+            dfs(i-1, j, n);
         }
     }
 }
