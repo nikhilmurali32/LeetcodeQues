1b6a4624251f41f1ecf45d6bee63cf5ab63ea5ce
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-02-26 00:34:41 -0500

    Time: 29 ms (5.05%) | Memory: 48.3 MB (6.30%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,43 @@
+class Solution {
+    boolean[] seen;
+    public int candy(int[] ratings) {
+        int n=ratings.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[1]-b[1]);
+        for(int i=0; i<n; i++){
+            pq.add(new int[]{i, ratings[i]});
+        }
+        seen = new boolean[n];
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);
+        int count=0;
+        while(count!=n){
+            int[] pair = pq.poll();
+            int minInd=pair[0];
+            int leftNei=minInd-1;
+            int rightNei=minInd+1;
+            if(leftNei>=0){
+                if(ratings[leftNei]<ratings[minInd]){
+                    candies[minInd] = candies[minInd]<=candies[leftNei]?candies[leftNei]+1:candies[minInd];
+                }
+                else if(ratings[leftNei]>ratings[minInd]){
+                    candies[leftNei] = candies[leftNei]<=candies[minInd]?candies[minInd]+1:candies[leftNei];
+                }
+            }
+            if(rightNei<n){
+                if(ratings[rightNei]<ratings[minInd]){
+                    candies[minInd] = candies[minInd]<=candies[rightNei]?candies[rightNei]+1:candies[minInd];
+                }
+                else if(ratings[rightNei]>ratings[minInd]){
+                    candies[rightNei] = candies[rightNei]<=candies[minInd]?candies[minInd]+1:candies[rightNei];
+                }
+            }
+            seen[minInd]=true;
+            count++;
+        }
+        int sum=0;
+        for(int i=0; i<candies.length; i++){
+            sum += candies[i];
+        }
+        return sum;
+    }
+}
