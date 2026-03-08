e9c2a5e89b4c091eff3193ee703db1ef09d5b29e
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-08 15:23:12 -0400

    Time: 2 ms (3.83%) | Memory: 45 MB (6.14%) - LeetSync

--- 4247-minimum-capacity-box/minimum-capacity-box.java
+++ 4247-minimum-capacity-box/minimum-capacity-box.java
@@ -1,16 +1,17 @@
 class Solution {
     public int minimumIndex(int[] capacity, int itemSize) {
         HashMap<Integer, Integer> hmap = new HashMap<>();
+        int min=Integer.MAX_VALUE;
         for(int i=0; i<capacity.length; i++){
             if(hmap.get(capacity[i])==null){
                 hmap.put(capacity[i], i);
             }
-        }
-        Arrays.sort(capacity);
-        for(int i=0; i<capacity.length; i++){
             if(capacity[i]>=itemSize){
-                return hmap.get(capacity[i]);
+                min=Math.min(min, capacity[i]);
             }
+        }
+        if(hmap.get(min)!=null){
+            return hmap.get(min);
         }
         return -1;
     }
