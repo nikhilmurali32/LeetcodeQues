5335ca4a4ebde5cfd6392a3c420c7b02a219509c
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-09 19:02:27 -0400

    Time: 7 ms (42.44%) | Memory: 45.4 MB (61.25%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -3,21 +3,21 @@ class Solution {
         Stack<Integer> st = new Stack<>();
         for(String c:tokens){
             if(c.equals("+") || c.equals("-") || c.equals("*") || c.equals("/")){
-                if(c.equals("/")){
-                    int b=st.pop();
-                    int a=st.pop();
-                    a=a/b;
-                    st.push(a);
-                } 
-                else if(c.equals("+")){
+                if(c.equals("+")){
                     st.push(st.pop()+ st.pop());
                 }
                 else if(c.equals("-")){
                     st.push(-1*st.pop()+ st.pop());
                 }
-                else{
+                else if(c.equals("*")){
                     st.push(st.pop()* st.pop());
-                }               
+                }
+                else{
+                    int b=st.pop();
+                    int a=st.pop();
+                    a=a/b;
+                    st.push(a);
+                }                
                 
             }
             else{
