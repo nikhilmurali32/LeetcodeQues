5214bf4c3a5c28aaf9ab1eee3127a38471cfdd56
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-09 18:57:09 -0400

    Time: 8 ms (13.85%) | Memory: 46.9 MB (8.13%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -3,21 +3,21 @@ class Solution {
         Deque<String> st = new ArrayDeque<>();
         for(String c:tokens){
             if(c.equals("+") || c.equals("-") || c.equals("*") || c.equals("/")){
-                if(c.equals("+")){
+                if(c.equals("/")){
+                    int b=Integer.valueOf(st.pop());
+                    int a=Integer.valueOf(st.pop());
+                    a=a/b;
+                    st.push(String.valueOf(a));
+                } 
+                else if(c.equals("+")){
                     st.push(String.valueOf(Integer.valueOf(st.pop())+ Integer.valueOf(st.pop())));
                 }
                 else if(c.equals("-")){
                     st.push(String.valueOf(-1*Integer.valueOf(st.pop())+ Integer.valueOf(st.pop())));
                 }
-                else if(c.equals("*")){
-                    st.push(String.valueOf(Integer.valueOf(st.pop())* Integer.valueOf(st.pop())));
-                }
                 else{
-                    int b=Integer.valueOf(st.pop());
-                    int a=Integer.valueOf(st.pop());
-                    a=a/b;
-                    st.push(String.valueOf(a));
-                }                
+                    st.push(String.valueOf(Integer.valueOf(st.pop())* Integer.valueOf(st.pop())));
+                }               
                 
             }
             else{
