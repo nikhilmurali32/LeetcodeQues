fd74bb623eed441c28e15ee136246756e9e100f7
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-11 18:10:52 -0400

    Time: 706 ms (5.16%) | Memory: 47.3 MB (11.19%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    List<List<Integer>> list = new ArrayList<>();
+    int n=0;
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        n=nums.length;
+        Arrays.sort(nums);
+        helper(nums, new ArrayList<>(), new boolean[n]);
+        return list;
+    }
+    public void helper(int[] nums, List<Integer> op, boolean[] b){
+        if(op.size()==n){
+            if(!list.contains(new ArrayList<>(op))){
+                list.add(new ArrayList<>(op));
+            }
+            return;
+        }
+        for(int i=0; i<n; i++){
+            if(b[i]){
+                continue;
+            }
+            op.add(nums[i]);
+            b[i]=true;
+            helper(nums, op, b);
+            op.remove(op.size()-1);
+            b[i]=false;
+        }
+    }
+}
