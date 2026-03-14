bfbe4ade0a5fd2fced9ced8f31d7ecd9dd21ad22
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-14 11:49:24 -0400

    Time: 110 ms (16.62%) | Memory: 77.8 MB (66.57%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,33 +1,34 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        if(heights.length==0){
-            return 0;
-        }
-        int[] lessFromLeft = new int[heights.length];
-        lessFromLeft[0]=-1;
-        for(int i=1; i<heights.length; i++){
-            int p=i-1;
-            while(p>=0 && heights[p]>=heights[i]){
-                p = lessFromLeft[p];
+        int n=heights.length;
+        Stack<Integer> st_left = new Stack<>();
+        int[] left_max = new int[n];
+        left_max[0]=1;
+        st_left.push(0);
+
+        Stack<Integer> st_right = new Stack<>();
+        int[] right_max = new int[n];
+        right_max[n-1]=1;
+        st_right.push(n-1);
+
+        for(int i=1; i<n; i++){
+            while(!st_left.isEmpty() && heights[i]<=heights[st_left.peek()]){
+                st_left.pop();
             }
-            lessFromLeft[i]=p;
-        }
+            left_max[i] = st_left.isEmpty()?i+1:i-st_left.peek();
+            st_left.push(i);
 
-        int[] lessFromRight = new int[heights.length];
-        lessFromRight[heights.length-1]=heights.length;
-        for (int i = heights.length - 2; i >= 0; i--) {
-            int p = i + 1;
-
-            while (p < heights.length && heights[p] >= heights[i]) {
-                p = lessFromRight[p];
+            while(!st_right.isEmpty() && heights[n-i-1]<=heights[st_right.peek()]){
+                st_right.pop();
             }
-            lessFromRight[i] = p;
+            right_max[n-i-1] = st_right.isEmpty()?i+1:st_right.peek()-(n-i-1);
+            st_right.push(n-i-1);
         }
 
-        int maxArea=0;
-        for(int i=0; i<heights.length; i++){
-            maxArea = Math.max(maxArea, heights[i]*(lessFromRight[i]-lessFromLeft[i]-1));
+        int max_area = 0;
+        for(int i=0; i<n; i++){
+            max_area = Math.max(max_area, heights[i]*(left_max[i]+right_max[i]-1));
         }
-        return maxArea;
+        return max_area;
     }
 }
