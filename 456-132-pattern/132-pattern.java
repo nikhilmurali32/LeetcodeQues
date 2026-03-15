7306e4443cb062f75f9ef886ea9d725d1d6c745f
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-14 20:50:51 -0400

    Time: 13 ms (96.18%) | Memory: 98.1 MB (76.57%) - LeetSync

--- 456-132-pattern/132-pattern.java
+++ 456-132-pattern/132-pattern.java
@@ -1,21 +1,16 @@
 class Solution {
     public boolean find132pattern(int[] nums) {
+        Deque<Integer> dq = new ArrayDeque<>();
         int n=nums.length;
-        int[] min_left = new int[n];
-        Arrays.fill(min_left, Integer.MAX_VALUE);
-        int min=nums[0];
-        for(int i=1; i<n; i++){
-            if(min<nums[i]){
-                min_left[i]=min;
-            }
-            min=Math.min(min, nums[i]);
-        }
-        TreeSet<Integer> treeSet = new TreeSet<>();
+        int third=Integer.MIN_VALUE;
         for(int i=n-1; i>=0; i--){
-            if(treeSet.lower(nums[i]) != null && treeSet.lower(nums[i]) > min_left[i]){
+            if(nums[i]<third){
                 return true;
             }
-            treeSet.add(nums[i]);
+            while(!dq.isEmpty() && nums[i]>dq.peekLast()){
+                third = dq.removeLast();
+            }
+            dq.addLast(nums[i]);
         }
         return false;
     }
