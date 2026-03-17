bb03133d998b091454ae063d689c182b81b5d91a
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-16 20:11:25 -0400

    Time: 15 ms (63.57%) | Memory: 51.1 MB (94.35%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -4,6 +4,9 @@ class Solution {
         Arrays.sort(s);
         int i=0, j=0;
         int count=0;
+        if(s.length==0 || g.length==0 || s[s.length-1]<g[0]){
+            return count;
+        }
         while(i<g.length && j<s.length){
             if(g[i]<=s[j]){
                 count++;
