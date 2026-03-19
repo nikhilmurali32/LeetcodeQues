1f8e35a1462393fa65bc688a2ef5a74439240946
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-19 11:16:50 -0400

    Time: 28 ms (14.57%) | Memory: 42.2 MB (77.53%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.java
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -1,45 +1,15 @@
 class Solution {
     public int minSteps(int n) {
-        if(n==1){
-            return 0;
-        }
-        if(isPrime(n)){
-            return n;
-        }
-        int[] dp = new int[n/2+1];
+        int[] dp = new int[n+1];
         dp[1]=0;
-        int end = lastFactor(n);
-        int i=2;
-        while(i<=end){
-            if( (i==2) || (i%2 != 0 && isPrime(i))){
-                dp[i]=i;
-            }
-            else{
-                int fact=lastFactor(i);
-                dp[i]=dp[fact] + 1 + (i/fact)-1;
-            }
-            i++;
-        }
-        return dp[i-1] + 1 + (n/end)-1;
-    }
-    public int lastFactor(int n){
-        if(n%2 == 0){
-            return n/2;
-        }
-        int start=n/2;
-        for(int i=start; i>0; i--){
-            if(n%i == 0){
-                return i;
+        for(int i=2; i<=n; i++){
+            dp[i]=i;
+            for(int j=1; j<=i/2; j++){
+                if(i%j==0){
+                    dp[i] = Math.min(dp[i], dp[j]+i/j);
+                }
             }
         }
-        return 1;
-    }
-    public boolean isPrime(int n){
-        for(int i=2; i<=Math.sqrt(n); i++){
-            if(n%i==0){
-                return false;
-            }
-        }
-        return true;
+        return dp[n];
     }
 }
