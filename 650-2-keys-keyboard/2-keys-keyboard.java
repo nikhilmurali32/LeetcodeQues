f6c9a3efa642787f9688c195dc839a22050b89c8
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-19 10:45:30 -0400

    Time: 1 ms (57.41%) | Memory: 42.3 MB (62.59%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.java
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -6,9 +6,11 @@ class Solution {
         if(isPrime(n)){
             return n;
         }
-        int[] dp = new int[n+1];
+        int[] dp = new int[n/2+1];
         dp[1]=0;
-        for(int i=2; i<=n; i++){
+        int end = lastFactor(n);
+        int i=2;
+        while(i<=end){
             if( (i==2) || (i%2 != 0 && isPrime(i))){
                 dp[i]=i;
             }
@@ -16,8 +18,9 @@ class Solution {
                 int fact=lastFactor(i);
                 dp[i]=dp[fact] + 1 + (i/fact)-1;
             }
+            i++;
         }
-        return dp[n];
+        return dp[i-1] + 1 + (n/end)-1;
     }
     public int lastFactor(int n){
         if(n%2 == 0){
