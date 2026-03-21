3743d88af9633e79d239737b0772e873891e897b
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-21 13:38:31 -0400

    Time: 0 ms (100.00%) | Memory: 42 MB (88.63%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,11 +1,5 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        // int[][] dp = new int[m][n];
-        // for(int[] arr:dp){
-        //     Arrays.fill(arr, -1);
-        // }
-        // return helper(m-1, n-1, dp);
-
         int left=1, tot=1;
         int[] top = new int[n];
         Arrays.fill(top, 1);
@@ -19,13 +13,4 @@ class Solution {
         }
         return tot;
     }
-    // public int helper(int i, int j, int[][] dp){
-    //     if(i==0 || j==0){
-    //         return dp[i][j]=1;
-    //     }
-    //     if(dp[i][j] != -1){
-    //         return dp[i][j];
-    //     }
-    //     return dp[i][j] = helper(i-1, j, dp)+helper(i, j-1, dp);
-    // }
 }
