324f8ad12ee8f59e50b37a0682ad0117fa782030
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-21 17:47:51 -0400

    Time: 5 ms (37.99%) | Memory: 45.2 MB (89.91%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -2,21 +2,19 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n=triangle.size();
         int[] dp = new int[n];
-        int[] dp_copy = new int[n];
         dp[0]=triangle.get(0).get(0);
         for(int i=1; i<n; i++){
-            for(int j=0; j<i+1; j++){
+            for(int j=i; j>=0; j--){
                 if(j==0){
-                    dp_copy[j] = triangle.get(i).get(0)+dp[j];
+                    dp[j] = triangle.get(i).get(0)+dp[j];
                 }
                 else if(j==i){
-                    dp_copy[j] = triangle.get(i).get(j)+dp[j-1];
+                    dp[j] = triangle.get(i).get(j)+dp[j-1];
                 }
                 else{
-                    dp_copy[j] = Math.min(triangle.get(i).get(j)+dp[j], triangle.get(i).get(j)+dp[j-1]);
+                    dp[j] = Math.min(triangle.get(i).get(j)+dp[j], triangle.get(i).get(j)+dp[j-1]);
                 }
             }
-            dp = Arrays.copyOfRange(dp_copy, 0, n);
         }
         int min=Integer.MAX_VALUE;
         for(int i=0; i<n; i++){
