b42eaf6a25969d6d1575264eb886e88833eeddfd
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-21 17:32:08 -0400

    Time: 5 ms (37.99%) | Memory: 45.6 MB (59.17%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -2,7 +2,6 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n=triangle.size();
         int[][] dp = new int[n][n];
-        // return helper(triangle, 0, 0, n, dp);
 
         dp[0][0]=triangle.get(0).get(0);
         for(int i=1; i<n; i++){
@@ -16,7 +15,6 @@ class Solution {
                 else{
                     dp[i][j] = Math.min(triangle.get(i).get(j)+dp[i-1][j], triangle.get(i).get(j)+dp[i-1][j-1]);
                 }
-                System.out.println(dp[i][j]);
             }
         }
         int min=Integer.MAX_VALUE;
@@ -25,13 +23,4 @@ class Solution {
         }
         return min;
     }
-    // public int helper(List<List<Integer>> triangle, int row, int ind, int n, Integer[][] dp){
-    //     if(row==n-1){
-    //         return dp[row][ind]=triangle.get(row).get(ind);
-    //     }
-    //     if(dp[row][ind] != null){
-    //         return dp[row][ind];
-    //     }
-    //     return dp[row][ind]=Math.min(triangle.get(row).get(ind) + helper(triangle, row+1, ind, n, dp), triangle.get(row).get(ind) + helper(triangle, row+1, ind+1, n, dp));
-    // }
 }
