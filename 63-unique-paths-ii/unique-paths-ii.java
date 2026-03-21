086ec573dd7450db15c2d0118256fa56160d82bb
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-21 14:12:37 -0400

    Time: 0 ms (100.00%) | Memory: 43.5 MB (76.42%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -3,32 +3,22 @@ class Solution {
         int m=obstacleGrid.length;
         int n=obstacleGrid[0].length;
         int[][] dp = new int[m][n];
-        for(int[] arr:dp){
-            Arrays.fill(arr, -1);
-        }
-        boolean seen=false;
         for(int i=0; i<n; i++){
-            if(obstacleGrid[0][i]==0 && !seen){
+            if(obstacleGrid[0][i]==0){
                 dp[0][i]=1;
             }
             else{
-                dp[0][i]=0;
-                seen=true;
+                break;
             }
         }
-        seen=false;
         for(int i=0; i<m; i++){
-            if(obstacleGrid[i][0]==0 && !seen){
+            if(obstacleGrid[i][0]==0){
                 dp[i][0]=1;
             }
             else{
-                dp[i][0]=0;
-                seen=true;
+                break;
             }
         }
-        // return helper(obstacleGrid, m-1, n-1, dp);
-
-
         for(int i=1; i<m; i++){
             for(int j=1; j<n; j++){
                 if(obstacleGrid[i][j]==1){
@@ -40,17 +30,5 @@ class Solution {
             }
         }
         return dp[m-1][n-1];        
-    }
-    public int helper(int[][] arr, int i, int j, int[][] dp){
-        if(i==0 || j==0){
-            return dp[i][j];
-        }
-        if((i>0 && j>0) && arr[i][j]==1){
-            return dp[i][j]=0;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        return dp[i][j] = helper(arr, i-1, j, dp) + helper(arr, i, j-1, dp);
     }
 }
