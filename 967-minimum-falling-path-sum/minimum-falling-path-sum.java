deeb0ab06c319f77b9873c917faf40ea8e600836
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-21 18:38:13 -0400

    Time: 11 ms (7.43%) | Memory: 46.9 MB (94.11%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,12 +1,7 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int n=matrix.length;
-        // int min=Integer.MAX_VALUE;
         Integer[][] dp = new Integer[n][n];
-        // for(int i=0; i<n; i++){
-        //     min = Math.min(min, helper(matrix, 0, i, n, dp));
-        // }
-        // return min;
 
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
@@ -32,19 +27,4 @@ class Solution {
         }
         return min;
     }
-    // public int helper(int[][] matrix, int row, int ind, int n, Integer[][] dp){
-    //     if(row==n-1){
-    //         return dp[row][ind]=matrix[row][ind];
-    //     }
-    //     if(dp[row][ind] != null){
-    //         return dp[row][ind];
-    //     }
-    //     if(ind==0){
-    //         return dp[row][ind]=Math.min(matrix[row][ind]+helper(matrix, row+1, ind, n, dp), matrix[row][ind]+helper(matrix, row+1, ind+1, n, dp));
-    //     }
-    //     else if(ind==n-1){
-    //         return dp[row][ind]=Math.min(matrix[row][ind]+helper(matrix, row+1, ind, n, dp), matrix[row][ind]+helper(matrix, row+1, ind-1, n, dp));
-    //     }
-    //     return dp[row][ind]=Math.min(matrix[row][ind]+helper(matrix, row+1, ind-1, n, dp), Math.min(matrix[row][ind]+helper(matrix, row+1, ind, n, dp), matrix[row][ind]+helper(matrix, row+1, ind+1, n, dp)));
-    // }
 }
