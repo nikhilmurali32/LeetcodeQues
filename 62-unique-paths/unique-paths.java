3158c1f5ea5209467bf17568db0b0b7d104ad07a
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-21 13:23:52 -0400

    Time: 0 ms (100.00%) | Memory: 42.2 MB (59.58%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,29 +1,18 @@
 class Solution {
-    int[][] dp;
     public int uniquePaths(int m, int n) {
-        dp = new int[m][n];
-        // dp[0][0]=1;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(i==0 || j==0){
-                    dp[i][j] = 1;
-                    continue;
-                }
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
+        int[][] dp = new int[m][n];
+        for(int[] arr:dp){
+            Arrays.fill(arr, -1);
         }
-        return dp[m-1][n-1];
+        return helper(m-1, n-1, dp);
     }
-    // public int helper(int m, int n, int i, int j){
-    //     if(i<0 || i>m-1 || j<0 || j>n-1){
-    //         return 0;
-    //     }
-    //     if(i==m-1 && j==n-1){
-    //         return 1;
-    //     }
-    //     if(dp[i][j] != 0){
-    //         return dp[i][j];
-    //     }
-    //     return dp[i][j]=helper(m, n, i+1, j)+helper(m, n, i, j+1);
-    // }
+    public int helper(int i, int j, int[][] dp){
+        if(i==0 || j==0){
+            return dp[i][j]=1;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        return dp[i][j] = helper(i-1, j, dp)+helper(i, j-1, dp);
+    }
 }
