2c2b39b25e6e501ef1d1f61d2b3b45d90cbb83c0
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-21 17:43:11 -0400

    Time: 5 ms (37.99%) | Memory: 45.5 MB (71.66%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,25 +1,26 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n=triangle.size();
-        int[][] dp = new int[n][n];
-
-        dp[0][0]=triangle.get(0).get(0);
+        int[] dp = new int[n];
+        int[] dp_copy = new int[n];
+        dp[0]=triangle.get(0).get(0);
         for(int i=1; i<n; i++){
             for(int j=0; j<i+1; j++){
                 if(j==0){
-                    dp[i][j] = triangle.get(i).get(0)+dp[i-1][j];
+                    dp_copy[j] = triangle.get(i).get(0)+dp[j];
                 }
                 else if(j==i){
-                    dp[i][j] = triangle.get(i).get(j)+dp[i-1][j-1];
+                    dp_copy[j] = triangle.get(i).get(j)+dp[j-1];
                 }
                 else{
-                    dp[i][j] = Math.min(triangle.get(i).get(j)+dp[i-1][j], triangle.get(i).get(j)+dp[i-1][j-1]);
+                    dp_copy[j] = Math.min(triangle.get(i).get(j)+dp[j], triangle.get(i).get(j)+dp[j-1]);
                 }
             }
+            dp = Arrays.copyOfRange(dp_copy, 0, n);
         }
         int min=Integer.MAX_VALUE;
         for(int i=0; i<n; i++){
-            min=Math.min(min, dp[n-1][i]);
+            min=Math.min(min, dp[i]);
         }
         return min;
     }
