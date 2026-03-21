89e049d1e358408a537d9d88fc9e254e83e4d74a
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-21 18:37:50 -0400

    Time: 11 ms (7.43%) | Memory: 47.1 MB (78.22%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,26 +1,50 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int n=matrix.length;
-        int min=Integer.MAX_VALUE;
+        // int min=Integer.MAX_VALUE;
         Integer[][] dp = new Integer[n][n];
+        // for(int i=0; i<n; i++){
+        //     min = Math.min(min, helper(matrix, 0, i, n, dp));
+        // }
+        // return min;
+
         for(int i=0; i<n; i++){
-            min = Math.min(min, helper(matrix, 0, i, n, dp));
+            for(int j=0; j<n; j++){
+                if(i==0){
+                    dp[i][j]=matrix[i][j];
+                }
+                else{
+                    if(j==0){
+                        dp[i][j] = matrix[i][j]+Math.min(dp[i-1][j], dp[i-1][j+1]);
+                    }
+                    else if(j==n-1){
+                        dp[i][j] = matrix[i][j]+Math.min(dp[i-1][j], dp[i-1][j-1]);
+                    }
+                    else{
+                        dp[i][j] = matrix[i][j]+Math.min(dp[i-1][j+1], Math.min(dp[i-1][j], dp[i-1][j-1]));
+                    }
+                }
+            }
+        }
+        int min=Integer.MAX_VALUE;
+        for(int i=0; i<n; i++){
+            min = Math.min(min, dp[n-1][i]);
         }
         return min;
     }
-    public int helper(int[][] matrix, int row, int ind, int n, Integer[][] dp){
-        if(row==n-1){
-            return dp[row][ind]=matrix[row][ind];
-        }
-        if(dp[row][ind] != null){
-            return dp[row][ind];
-        }
-        if(ind==0){
-            return dp[row][ind]=Math.min(matrix[row][ind]+helper(matrix, row+1, ind, n, dp), matrix[row][ind]+helper(matrix, row+1, ind+1, n, dp));
-        }
-        else if(ind==n-1){
-            return dp[row][ind]=Math.min(matrix[row][ind]+helper(matrix, row+1, ind, n, dp), matrix[row][ind]+helper(matrix, row+1, ind-1, n, dp));
-        }
-        return dp[row][ind]=Math.min(matrix[row][ind]+helper(matrix, row+1, ind-1, n, dp), Math.min(matrix[row][ind]+helper(matrix, row+1, ind, n, dp), matrix[row][ind]+helper(matrix, row+1, ind+1, n, dp)));
-    }
+    // public int helper(int[][] matrix, int row, int ind, int n, Integer[][] dp){
+    //     if(row==n-1){
+    //         return dp[row][ind]=matrix[row][ind];
+    //     }
+    //     if(dp[row][ind] != null){
+    //         return dp[row][ind];
+    //     }
+    //     if(ind==0){
+    //         return dp[row][ind]=Math.min(matrix[row][ind]+helper(matrix, row+1, ind, n, dp), matrix[row][ind]+helper(matrix, row+1, ind+1, n, dp));
+    //     }
+    //     else if(ind==n-1){
+    //         return dp[row][ind]=Math.min(matrix[row][ind]+helper(matrix, row+1, ind, n, dp), matrix[row][ind]+helper(matrix, row+1, ind-1, n, dp));
+    //     }
+    //     return dp[row][ind]=Math.min(matrix[row][ind]+helper(matrix, row+1, ind-1, n, dp), Math.min(matrix[row][ind]+helper(matrix, row+1, ind, n, dp), matrix[row][ind]+helper(matrix, row+1, ind+1, n, dp)));
+    // }
 }
