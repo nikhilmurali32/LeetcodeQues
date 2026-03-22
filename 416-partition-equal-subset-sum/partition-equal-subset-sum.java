9c35b2d35799128eb2d9eeae00c695ba57924655
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-22 18:31:27 -0400

    Time: 64 ms (37.07%) | Memory: 47.8 MB (50.13%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -9,11 +9,6 @@ class Solution {
             return false;
         }
         target=target/2;
-        // int[][] dp = new int[n+1][target+1];
-        // for(int[] arr:dp){
-        //     Arrays.fill(arr, -1);
-        // }
-        // return doesExist(nums, target, n-1, dp)==1?false:true;
         boolean[][] dp = new boolean[n+1][target+1];
         dp[0][0]=true;
         for(int i=1; i<=n; i++){
@@ -32,31 +27,5 @@ class Solution {
             }
         }
         return dp[n][target];
-    }
-    public int doesExist(int[] nums, int target, int ind, int[][] dp){
-        if(target==0){
-            return dp[ind][target]=0;
-        }
-        if(dp[ind][target] != -1){
-            return dp[ind][target];
-        }
-        if(ind==0){
-            if(target==nums[ind]){
-                dp[ind][target]=0;
-            }
-            else{
-                dp[ind][target]=1;
-            }
-            return dp[ind][target];
-        }
-        int doNotTake = doesExist(nums, target, ind-1, dp);
-        int take=1;
-        if(target>=nums[ind]){
-            take = doesExist(nums, target-nums[ind], ind-1, dp);
-        }
-        if(doNotTake==0 || take==0){
-            return dp[ind][target]=0;
-        }
-        return dp[ind][target]=1;
     }
 }
