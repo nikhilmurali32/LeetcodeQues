669120893cd3b5f0b5dfaae2fe17d505ecba7f86
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-22 18:31:01 -0400

    Time: 64 ms (37.07%) | Memory: 47.4 MB (62.23%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -9,11 +9,29 @@ class Solution {
             return false;
         }
         target=target/2;
-        int[][] dp = new int[n][target+1];
-        for(int[] arr:dp){
-            Arrays.fill(arr, -1);
+        // int[][] dp = new int[n+1][target+1];
+        // for(int[] arr:dp){
+        //     Arrays.fill(arr, -1);
+        // }
+        // return doesExist(nums, target, n-1, dp)==1?false:true;
+        boolean[][] dp = new boolean[n+1][target+1];
+        dp[0][0]=true;
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<=target; j++){
+                if(j==0){
+                    dp[i][j]=true;
+                }
+                else{
+                    boolean doNotTake = dp[i-1][j];
+                    boolean take=false;
+                    if(j>=nums[i-1]){
+                        take = dp[i-1][j-nums[i-1]];
+                    }
+                    dp[i][j] = doNotTake || take;
+                }
+            }
         }
-        return doesExist(nums, target, n-1, dp)==1?false:true;
+        return dp[n][target];
     }
     public int doesExist(int[] nums, int target, int ind, int[][] dp){
         if(target==0){
