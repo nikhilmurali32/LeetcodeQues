3860ce03791653c193ef1123222d89dfb17972a6
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-26 16:10:12 -0400

    Time: 7 ms (36.35%) | Memory: 46.1 MB (93.83%) - LeetSync

--- 1184-car-pooling/car-pooling.java
+++ 1184-car-pooling/car-pooling.java
@@ -1,26 +1,26 @@
 class Solution {
-    class Node{
-        int numPassengers;
-        int from;
-        int to;
-        Node(int numPassengers, int from, int to){
-            this.numPassengers=numPassengers;
-            this.from=from;
-            this.to=to;
-        }
-    }
+    // class Node{
+    //     int numPassengers;
+    //     int from;
+    //     int to;
+    //     Node(int numPassengers, int from, int to){
+    //         this.numPassengers=numPassengers;
+    //         this.from=from;
+    //         this.to=to;
+    //     }
+    // }
     public boolean carPooling(int[][] trips, int capacity) {
         Arrays.sort(trips, (a,b)->a[1]==b[1]?a[2]-b[2]:a[1]-b[1]);
-        PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> a.to-b.to);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[2]-b[2]);
         for(int i=0; i<trips.length; i++){
-            while(!pq.isEmpty() && trips[i][1]>=pq.peek().to){
-                capacity += pq.remove().numPassengers;
+            while(!pq.isEmpty() && trips[i][1]>=pq.peek()[2]){
+                capacity += pq.remove()[0];
             }
             if(capacity<trips[i][0]){
                 return false;
             }
             capacity -= trips[i][0];
-            pq.add(new Node(trips[i][0], trips[i][1], trips[i][2]));
+            pq.add(new int[]{trips[i][0], trips[i][1], trips[i][2]});
         }
         return true;
     }
