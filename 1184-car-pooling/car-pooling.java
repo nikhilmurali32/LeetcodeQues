8c9aeffafd12edd4c80274da9027d5957549430c
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-26 16:00:36 -0400

    Time: 8 ms (18.61%) | Memory: 46.4 MB (37.11%) - LeetSync

---
+++ 1184-car-pooling/car-pooling.java
@@ -0,0 +1,27 @@
+class Solution {
+    class Node{
+        int numPassengers;
+        int from;
+        int to;
+        Node(int numPassengers, int from, int to){
+            this.numPassengers=numPassengers;
+            this.from=from;
+            this.to=to;
+        }
+    }
+    public boolean carPooling(int[][] trips, int capacity) {
+        Arrays.sort(trips, (a,b)->a[1]==b[1]?a[2]-b[2]:a[1]-b[1]);
+        PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> a.to-b.to);
+        for(int i=0; i<trips.length; i++){
+            while(!pq.isEmpty() && trips[i][1]>=pq.peek().to){
+                capacity += pq.remove().numPassengers;
+            }
+            if(capacity<trips[i][0]){
+                return false;
+            }
+            capacity -= trips[i][0];
+            pq.add(new Node(trips[i][0], trips[i][1], trips[i][2]));
+        }
+        return true;
+    }
+}
