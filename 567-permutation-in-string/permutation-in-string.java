e7cfcc82f4b03aefa9bcc72d8172afd047a9673e
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-27 19:01:12 -0400

    Time: 10 ms (39.02%) | Memory: 44.2 MB (47.32%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,24 +1,27 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        HashMap<Character, Integer> freq = new HashMap<>();
+        int[] freq = new int[26];
+        HashSet<Character> hset = new HashSet<>();
         for(char ch :s1.toCharArray()){
-            freq.put(ch, freq.getOrDefault(ch,0)+1);
+            freq[ch-'a']++;
+            hset.add(ch);
         }
-        int i=0, j=0, count=freq.size();
+        int count=hset.size();
+        int i=0, j=0;
         while(j<s2.length()){
             char ch=s2.charAt(j);
-            if(freq.containsKey(ch)){
-                freq.put(ch, freq.get(ch)-1);
-                if(freq.get(ch)==0){
+            if(hset.contains(ch)){
+                freq[ch-'a']--;
+                if(freq[ch-'a']==0){
                     count--;
                 }                
             }
 
             while(count==0){
                 char c=s2.charAt(i);
-                if(freq.containsKey(c)){
-                    freq.put(c, freq.get(c)+1);
-                    if(freq.get(c)>0){
+                if(hset.contains(c)){
+                    freq[c-'a']++;
+                    if(freq[c-'a']>0){
                         count++;
                     }
                 }
