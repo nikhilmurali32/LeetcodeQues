e6c67b30aaa5f1a9ca858583c7d0502937e55448
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-30 23:41:48 -0400

    Time: 22 ms (33.59%) | Memory: 46.9 MB (35.94%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -2,10 +2,7 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
         int[][] dp = new int[n+1][amount+1];
-        // for(int[] arr:dp){
-        //     Arrays.fill(arr, -1);
-        // }
-        // Arrays.sort(coins);
+
         for(int i=0; i<=coins.length; i++){
             for(int j=1; j<=amount; j++){
                 if(i==0){
@@ -21,24 +18,5 @@ class Solution {
             }
         }
         return dp[n][amount]==100000?-1:dp[n][amount];
-        // int ans=helper(coins, amount, coins.length-1, dp);
-        // return ans==100000?-1:ans;
     }
-    // public int helper(int[] coins, int amount, int ind, int[][] dp){
-    //     if(amount==0){
-    //         return dp[ind][amount]=0;
-    //     }
-    //     if(ind<0){
-    //         return 100000;
-    //     }
-    //     if(dp[ind][amount] != -1){
-    //         return dp[ind][amount];
-    //     }
-    //     int doNotTake = helper(coins, amount, ind-1, dp);
-    //     int take=100000;
-    //     if(coins[ind]<=amount){
-    //         take = 1+helper(coins, amount-coins[ind], ind, dp);
-    //     }
-    //     return dp[ind][amount]=Math.min(take, doNotTake);
-    // }
 }
