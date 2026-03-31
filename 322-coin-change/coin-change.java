2d28081e22c2e2cd15c3fac5fda1816f37da570a
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-30 23:46:43 -0400

    Time: 17 ms (47.27%) | Memory: 47.2 MB (22.91%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -2,13 +2,11 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
         int[][] dp = new int[n+1][amount+1];
-
-        for(int i=0; i<=coins.length; i++){
+        for(int j=1; j<=amount; j++){
+            dp[0][j] = 100000;
+        }
+        for(int i=1; i<=coins.length; i++){
             for(int j=1; j<=amount; j++){
-                if(i==0){
-                    dp[i][j] = 100000;
-                    continue;
-                }
                 int doNotTake = dp[i-1][j];
                 int take = 100000;
                 if(coins[i-1]<=j){
