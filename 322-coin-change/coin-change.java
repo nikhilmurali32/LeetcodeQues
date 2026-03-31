7af6166d542b23aa53aa55364d7dff27693868bc
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-03-30 23:41:20 -0400

    Time: 25 ms (30.53%) | Memory: 47 MB (27.68%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,28 +1,44 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[][] dp = new int[coins.length][amount+1];
-        for(int[] arr:dp){
-            Arrays.fill(arr, -1);
+        int n=coins.length;
+        int[][] dp = new int[n+1][amount+1];
+        // for(int[] arr:dp){
+        //     Arrays.fill(arr, -1);
+        // }
+        // Arrays.sort(coins);
+        for(int i=0; i<=coins.length; i++){
+            for(int j=1; j<=amount; j++){
+                if(i==0){
+                    dp[i][j] = 100000;
+                    continue;
+                }
+                int doNotTake = dp[i-1][j];
+                int take = 100000;
+                if(coins[i-1]<=j){
+                    take = 1 + dp[i][j-coins[i-1]];
+                }
+                dp[i][j] = Math.min(doNotTake, take);
+            }
         }
-        Arrays.sort(coins);
-        int ans=helper(coins, amount, coins.length-1, dp);
-        return ans==100000?-1:ans;
+        return dp[n][amount]==100000?-1:dp[n][amount];
+        // int ans=helper(coins, amount, coins.length-1, dp);
+        // return ans==100000?-1:ans;
     }
-    public int helper(int[] coins, int amount, int ind, int[][] dp){
-        if(amount==0){
-            return dp[ind][amount]=0;
-        }
-        if(ind<0){
-            return 100000;
-        }
-        if(dp[ind][amount] != -1){
-            return dp[ind][amount];
-        }
-        int doNotTake = helper(coins, amount, ind-1, dp);
-        int take=100000;
-        if(coins[ind]<=amount){
-            take = 1+helper(coins, amount-coins[ind], ind, dp);
-        }
-        return dp[ind][amount]=Math.min(take, doNotTake);
-    }
+    // public int helper(int[] coins, int amount, int ind, int[][] dp){
+    //     if(amount==0){
+    //         return dp[ind][amount]=0;
+    //     }
+    //     if(ind<0){
+    //         return 100000;
+    //     }
+    //     if(dp[ind][amount] != -1){
+    //         return dp[ind][amount];
+    //     }
+    //     int doNotTake = helper(coins, amount, ind-1, dp);
+    //     int take=100000;
+    //     if(coins[ind]<=amount){
+    //         take = 1+helper(coins, amount-coins[ind], ind, dp);
+    //     }
+    //     return dp[ind][amount]=Math.min(take, doNotTake);
+    // }
 }
