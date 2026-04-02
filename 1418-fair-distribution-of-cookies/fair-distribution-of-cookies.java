98311f7c195352b83d4c843ecd028cb4bc7727b8
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-04-02 03:13:09 -0400

    Time: 449 ms (5.01%) | Memory: 42.4 MB (95.47%) - LeetSync

---
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.java
@@ -0,0 +1,28 @@
+class Solution {
+    int minUnf;
+    public int distributeCookies(int[] cookies, int k) {
+        // Arrays.sort(cookies);
+        minUnf = Integer.MAX_VALUE;
+        int n=cookies.length;
+        int[] children = new int[k];
+        helper(cookies, n-1, children, k);
+        return minUnf;
+    }
+    public void helper(int[] cookies, int ind, int[] children, int k){
+        if(ind==-1){
+            int maxCookies=0;
+            for(int child:children){
+                if(child>maxCookies){
+                    maxCookies=child;
+                }
+            }
+            minUnf = Math.min(minUnf, maxCookies);
+            return;
+        }
+        for(int i=0; i<k; i++){
+            children[i] += cookies[ind];
+            helper(cookies, ind-1, children, k);
+            children[i] -= cookies[ind];
+        }
+    }
+}
