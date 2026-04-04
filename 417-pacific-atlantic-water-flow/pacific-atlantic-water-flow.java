47bfb14ea45e0e062b6baa8c1ac8a1e5ef1d22c7
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-04-04 14:22:46 -0400

    Time: 7 ms (38.55%) | Memory: 47.1 MB (93.65%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -1,24 +1,23 @@
 class Solution {
-    List<List<Integer>> list = new ArrayList<>();
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        List<List<Integer>> list = new ArrayList<>();
         int m=heights.length, n=heights[0].length;
-        HashSet<int[]> indices = new HashSet<>();
         boolean[][][] visited = new boolean[m][n][2];
         for(int i=0; i<m; i++){
-            dfs(heights, i, 0, 0, m, n, visited, 0);
+            dfs(heights, i, 0, 0, m, n, visited, 0, list);
         }
         for(int j=0; j<n; j++){
-            dfs(heights, 0, j, 0, m, n, visited, 0);
+            dfs(heights, 0, j, 0, m, n, visited, 0, list);
         }
         for(int i=0; i<m; i++){
-            dfs(heights, i, n-1, 0, m, n, visited, 1);
+            dfs(heights, i, n-1, 0, m, n, visited, 1, list);
         }
         for(int j=0; j<n; j++){
-            dfs(heights, m-1, j, 0, m, n, visited, 1);
+            dfs(heights, m-1, j, 0, m, n, visited, 1, list);
         }
         return list;
     }
-    public void dfs(int[][] heights, int i, int j, int prevHeight, int m, int n, boolean[][][] visited, int ocean){
+    public void dfs(int[][] heights, int i, int j, int prevHeight, int m, int n, boolean[][][] visited, int ocean, List<List<Integer>> list){
         if(i<0 || i>m-1 || j<0 || j>n-1 || heights[i][j]<prevHeight || visited[i][j][ocean]){
             return;
         }
@@ -29,9 +28,9 @@ class Solution {
             list.add(new ArrayList<>(ind));
         }
         visited[i][j][ocean]=true;
-        dfs(heights, i, j+1, heights[i][j], m, n, visited, ocean);
-        dfs(heights, i+1, j, heights[i][j], m, n, visited, ocean);
-        dfs(heights, i, j-1, heights[i][j], m, n, visited, ocean);
-        dfs(heights, i-1, j, heights[i][j], m, n, visited, ocean);
+        dfs(heights, i, j+1, heights[i][j], m, n, visited, ocean, list);
+        dfs(heights, i+1, j, heights[i][j], m, n, visited, ocean, list);
+        dfs(heights, i, j-1, heights[i][j], m, n, visited, ocean, list);
+        dfs(heights, i-1, j, heights[i][j], m, n, visited, ocean, list);
     }
 }
