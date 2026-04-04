8d2ad554a83468e2259ead89bdba39c3d095065f
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-04-04 15:59:23 -0400

    Time: 12 ms (95.94%) | Memory: 47.7 MB (49.76%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -7,8 +7,6 @@ class Solution {
         }
         zeroIndices.add(new int[]{0,0});
         int count=0;
-        boolean[][] visited = new boolean[m][n];
-        visited[0][0]=true;
         while(!zeroIndices.isEmpty()){
             int size = zeroIndices.size();
             for(int i=0; i<size; i++){
@@ -20,10 +18,10 @@ class Solution {
                 }
                 for(int k=-1; k<=1; k++){
                     for(int j=-1; j<=1; j++){
-                        if((k==0 && j==0) || x+k<0 || y+j<0 || x+k>=m || y+j>=n || grid[x+k][y+j]==1 || visited[x+k][y+j]){
+                        if((k==0 && j==0) || x+k<0 || y+j<0 || x+k>=m || y+j>=n || grid[x+k][y+j]==1){
                             continue;
                         }
-                        visited[x+k][y+j]=true;
+                        grid[x+k][y+j]=1;
                         zeroIndices.add(new int[]{x+k, y+j});
                     }
                 }
