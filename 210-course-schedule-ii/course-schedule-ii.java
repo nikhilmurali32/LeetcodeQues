f4311f6db3432cd1b77bda3fe69a886ba2ee1a51
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-04-04 21:44:18 -0400

    Time: 5 ms (82.58%) | Memory: 46.8 MB (62.10%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,39 +1,37 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int[][] vis = new int[numCourses][numCourses];
-        int[] inDegrees = new int[numCourses];
-        for(int i=0; i<prerequisites.length; i++){
-            int pre=prerequisites[i][1];
-            int curr=prerequisites[i][0];
-            if(vis[pre][curr]==0){
-                inDegrees[curr]++;
-            }
-            vis[pre][curr]=1;
+        List<List<Integer>> adjList = new ArrayList<>(numCourses);
+        int[] order = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            adjList.add(new ArrayList<>());
         }
-        Queue<Integer> q = new LinkedList<>();
+        int[] inDegrees = new int[numCourses];
+        for(int[] pre:prerequisites){
+            adjList.get(pre[1]).add(pre[0]);
+            inDegrees[pre[0]]++;
+        }
+        Queue<Integer> canTake = new LinkedList<>();
+        int count=0;
         for(int i=0; i<numCourses; i++){
             if(inDegrees[i]==0){
-                q.add(i);
+                count++;
+                canTake.add(i);
             }
         }
-        int[] ans=new int[numCourses];
-        int k=0;
-        while(!q.isEmpty()){
-            int curr=q.poll();
-            ans[k]=curr;
-            k++;
-            for(int i=0; i<numCourses; i++){
-                if(vis[curr][i]==1){
-                    inDegrees[i]--;
-                    if(inDegrees[i]==0){
-                        q.add(i);
-                    }
+        int ind=0, sum=0;
+        while(!canTake.isEmpty()){
+            int curr = canTake.remove();
+            sum += curr;
+            order[ind]=curr;
+            ind++;
+            for(int i:adjList.get(curr)){
+                inDegrees[i]--;
+                if(inDegrees[i]==0){
+                    count++;
+                    canTake.add(i);
                 }
             }
         }
-        if(k!=numCourses){
-            return new int[0];
-        }
-        return ans;       
+        return sum==(numCourses*(numCourses-1)/2)?order:new int[0];      
     }
 }
