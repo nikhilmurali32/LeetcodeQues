f89a800966c001599a5de00f7487ea9140eafd14
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-04-06 12:48:15 -0400

    Time: 11 ms (67.39%) | Memory: 49.4 MB (39.60%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -7,31 +7,30 @@ class Solution {
         for(int[] time:times){
             adjList.get(time[0]).add(new int[]{time[1], time[2]});
         }
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1]-b[1]);
-        pq.offer(new int[]{k, 0});
-        int[] dist = new int[n+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[k]=0;
-        while(!pq.isEmpty()){
-            int[] curr = pq.poll();
-            int node=curr[0];
-            int time=curr[1];
-            if(time>dist[node]){
-                continue;
-            }
+        PriorityQueue<int[]> leastTime = new PriorityQueue<>((a,b)->a[1]-b[1]);
+        leastTime.add(new int[]{k, 0});
+        int[] totalTime = new int[n+1];
+        Arrays.fill(totalTime, Integer.MAX_VALUE);
+        totalTime[k] = 0;
+        int maxTime=0;
+        while(!leastTime.isEmpty()){
+            int[] currNode = leastTime.remove();
+            int node = currNode[0];
+            int time = currNode[1];
             for(int[] nei:adjList.get(node)){
-                if(time+nei[1]<dist[nei[0]]){
-                    dist[nei[0]] = time+nei[1];
-                    pq.offer(new int[]{nei[0], nei[1]+time});
+                if(time + nei[1] < totalTime[nei[0]]){
+                    totalTime[nei[0]] = time + nei[1];
+                    leastTime.add(new int[]{nei[0], time + nei[1]});
                 }
             }
         }
-        int max = 0;
-        for(int i = 1; i <= n; i++){
-            if(dist[i] == Integer.MAX_VALUE) return -1;
-            max = Math.max(max, dist[i]);
+        for(int i=1; i<=n; i++){
+            if(totalTime[i]==Integer.MAX_VALUE){
+                return -1;
+            }
+            maxTime = Math.max(maxTime, totalTime[i]);
         }
+        return maxTime==0?-1:maxTime;
         
-        return max;
     }
 }
