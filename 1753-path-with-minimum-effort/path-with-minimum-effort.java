f853532d533e1fd15387669b652bb1f56b42b328
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-04-06 14:56:57 -0400

    Time: 39 ms (84.92%) | Memory: 48 MB (32.70%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -10,7 +10,6 @@ class Solution {
         PriorityQueue<int[]> leastEffort = new PriorityQueue<>((a,b)->a[2]-b[2]);
         leastEffort.add(new int[]{0, 0, 0});
         int res = 0;
-        int max=0;
         while(!leastEffort.isEmpty()){
             int[] currCell = leastEffort.remove();
             int currDist = currCell[2];
@@ -18,16 +17,19 @@ class Solution {
             if(currDist>minDistance[i][j]){
                 continue;
             }
-            max=Math.max(max, currDist);
             if(i==m-1 && j==n-1){
-                res=max;
+                res=currDist;
                 break;
             }
             for(int[] dir:directions){
                 int x=i+dir[0], y=j+dir[1];
-                if(x>=0 && x<m && y>=0 && y<n && Math.abs(heights[i][j]-heights[x][y])<minDistance[x][y]){
-                    minDistance[x][y] = Math.abs(heights[i][j]-heights[x][y]);
-                    leastEffort.add(new int[]{x, y, Math.abs(heights[i][j]-heights[x][y])});
+                if(x>=0 && x<m && y>=0 && y<n){
+                    int jumpEffort = Math.abs(heights[i][j]-heights[x][y]);
+                    int newPathEffort = Math.max(currDist, jumpEffort);
+                    if(newPathEffort<minDistance[x][y]){
+                        minDistance[x][y] = newPathEffort;
+                        leastEffort.add(new int[]{x,y,newPathEffort});
+                    }
                 }
             }
         }
