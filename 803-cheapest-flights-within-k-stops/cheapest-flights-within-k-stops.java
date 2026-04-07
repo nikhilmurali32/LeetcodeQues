0903eafa4c8d4d9ed400934a76d1ec0159879fd9
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-04-06 20:55:35 -0400

    Time: 6 ms (86.43%) | Memory: 46.6 MB (58.56%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -7,29 +7,22 @@ class Solution {
         for(int[] flight:flights){
             adjList.get(flight[0]).add(new int[]{flight[1], flight[2]});
         }
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->(a[1]-b[1]));
-        pq.offer(new int[]{src, 0, 0});
-        int[] minStops = new int[n];
-        Arrays.fill(minStops, Integer.MAX_VALUE);
-        while(!pq.isEmpty()){
-            int[] curr=pq.poll();
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{src, 0, 0});
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        while(!q.isEmpty()){
+            int[] curr=q.poll();
             int from=curr[0];
             int cost=curr[1];
             int stops = curr[2];
-            if(from==dst){
-                return cost;
-            }
-            if(minStops[from]<stops){
-                continue;
-            }
-            minStops[from]=stops;
             for(int[] nei:adjList.get(from)){
-                if(stops<=k){
-                    // dist[nei[0]]=cost+nei[1];
-                    pq.add(new int[]{nei[0], cost+nei[1], stops+1});
+                if(cost+nei[1] < dist[nei[0]] && stops<=k){
+                    dist[nei[0]]=cost+nei[1];
+                    q.add(new int[]{nei[0], cost+nei[1], stops+1});
                 }
             }
         }
-        return -1;
+        return dist[dst]==Integer.MAX_VALUE?-1:dist[dst];
     }
 }
