75353216fdb4ac5b72b3cb84881c307dae755a69
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-04-10 18:21:13 -0400

    Time: 10 ms (10.69%) | Memory: 48.2 MB (5.15%) - LeetSync

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    List<TreeNode> op_p;
+    List<TreeNode> op_q;
+    int count=0;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null){
+            return null;
+        }
+        if(root.val==p.val && root.val==q.val){
+            return root;
+        }
+        List<TreeNode> op = new ArrayList<>();
+        if(root.val==p.val){
+            op_p = new ArrayList<>(op);
+            op_p.add(root);
+            count++;
+        }
+        if(root.val==q.val){
+            op_q = new ArrayList<>(op);
+            op_q.add(root);
+            count++;
+        }
+        op.add(root);
+        dfs(root.left, op, p, q);
+        dfs(root.right, op, p, q);
+        int i=0;
+        while(i<Math.min(op_p.size(), op_q.size()) && op_p.get(i).val==op_q.get(i).val){
+            i++;
+        }
+        return op_p.get(i-1);
+
+    }
+    public void dfs(TreeNode node, List<TreeNode> op, TreeNode p, TreeNode q){
+        if(node==null || count==2){
+            return;
+        }
+        // System.out.println(node.val);
+        if(node.val==p.val){
+            op_p = new ArrayList<>(op);
+            op_p.add(node);
+            count++;
+        }
+        if(node.val==q.val){
+            op_q = new ArrayList<>(op);
+            op_q.add(node);
+            count++;
+        }
+        if(count==2){
+            return;
+        }
+        
+        op.add(node);
+        dfs(node.left, op, p, q);
+        dfs(node.right, op, p, q);
+        op.remove(op.size()-1);
+    }
+}
