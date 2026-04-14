bde1160a4e70de6eed19892c21d02b1a0f9aa8e9
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-04-14 12:19:46 -0400

    Time: 3 ms (92.72%) | Memory: 44.5 MB (68.72%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -4,20 +4,32 @@ class Solution {
         if(len1 != len2){
             return false;
         }
-        HashMap<Character, Integer> freqMap = new HashMap<>();
-        for(char ch:s.toCharArray()){
-            freqMap.put(ch, freqMap.getOrDefault(ch, 0)+1);
-        }
+        // HashMap<Character, Integer> freqMap = new HashMap<>();
+        int[] freqArr = new int[26];
         int count=0;
-        for(char ch:t.toCharArray()){
-            if(!freqMap.containsKey(ch)){
-                return false;
-            }
-            freqMap.put(ch, freqMap.get(ch)-1);
-            if(freqMap.get(ch)==0){
+        for(char ch:s.toCharArray()){
+            // freqMap.put(ch, freqMap.getOrDefault(ch, 0)+1);
+            if(freqArr[ch-'a']==0){
                 count++;
             }
+            freqArr[ch-'a']++;
         }
-        return count==freqMap.size();
+        for(char ch:t.toCharArray()){
+            // if(!freqMap.containsKey(ch)){
+            //     return false;
+            // }
+            if(freqArr[ch-'a']==0){
+                return false;
+            }
+            // freqMap.put(ch, freqMap.get(ch)-1);
+            freqArr[ch-'a']--;
+            // if(freqMap.get(ch)==0){
+            //     count++;
+            // }
+            if(freqArr[ch-'a']==0){
+                count--;
+            }
+        }
+        return count==0;
     }
 }
