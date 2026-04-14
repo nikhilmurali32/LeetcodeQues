8409d354c76349f432dd4b0dc2282e63b583ebdd
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-04-14 13:36:16 -0400

    Time: 12 ms (43.95%) | Memory: 47.4 MB (14.25%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -3,16 +3,29 @@ class Solution {
         int[] res = new int[2];
         HashMap<Integer, Integer> indicesMap = new HashMap<>();
         for(int i=0; i<nums.length; i++){
-            if(indicesMap.containsKey(nums[i]) && nums[i]+nums[i]==target){
-                res[0]=i;
-                res[1]=indicesMap.get(nums[i]);
-                return res;
+            if(indicesMap.containsKey(nums[i])){
+                if(nums[i]+nums[i]==target){
+                    res[0]=i;
+                    res[1]=indicesMap.get(nums[i]);
+                    return res;
+                }
+                else{
+                    continue;
+                }
             }
             indicesMap.put(nums[i], i);
         }
         Arrays.sort(nums);
         int l=0, r=nums.length-1;
         while(l<r){
+            if(l>0 && nums[l]==nums[l-1]){
+                l++;
+                continue;
+            }
+            if(r<nums.length-1 && nums[r]==nums[r+1]){
+                r--;
+                continue;
+            }
             int sum = nums[l]+nums[r];
             if(sum==target){
                 res[0]=indicesMap.get(nums[l]);
