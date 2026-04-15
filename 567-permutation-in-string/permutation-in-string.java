aa0ae6fd13a1b4bd950c52c798da163a3178706c
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-04-15 12:34:23 -0400

    Time: 6 ms (87.69%) | Memory: 44.2 MB (53.53%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,35 +1,27 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int[] freq = new int[26];
-        HashSet<Character> hset = new HashSet<>();
-        for(char ch :s1.toCharArray()){
-            freq[ch-'a']++;
-            hset.add(ch);
+        int m=s1.length(), n=s2.length();
+        if(m>n){
+            return false;
         }
-        int count=hset.size();
+        int[] freqMaps1 = new int[26];
+        for(char ch:s1.toCharArray()){
+            freqMaps1[ch-'a']++;
+        }
+        int[] freqMaps2 = new int[26];
         int i=0, j=0;
-        while(j<s2.length()){
+        while(i<=j && j<n){
             char ch=s2.charAt(j);
-            if(hset.contains(ch)){
-                freq[ch-'a']--;
-                if(freq[ch-'a']==0){
-                    count--;
-                }                
+            freqMaps2[ch-'a']++;
+            if(j-i+1 < m){
+                j++;                
+                continue;
+            }            
+            if(Arrays.equals(freqMaps1, freqMaps2)){
+                return true;
             }
-
-            while(count==0){
-                char c=s2.charAt(i);
-                if(hset.contains(c)){
-                    freq[c-'a']++;
-                    if(freq[c-'a']>0){
-                        count++;
-                    }
-                }
-                if(j-i+1==s1.length()){
-                    return true;
-                }
-                i++;
-            }
+            freqMaps2[s2.charAt(i)-'a']--;
+            i++;
             j++;
         }
         return false;
