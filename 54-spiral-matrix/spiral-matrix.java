3abe02f353792c0d83baa06fc9b753b993c205cb
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-04-16 14:25:08 -0400

    Time: 0 ms (100.00%) | Memory: 43.3 MB (12.20%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,53 +1,36 @@
 class Solution {
-    List<Integer> list;
-    boolean[][] dp;
-    char ch='r';
-    boolean turns=false;
     public List<Integer> spiralOrder(int[][] matrix) {
-        list=new ArrayList<>();
-        int m=matrix.length;
-        int n=matrix[0].length;
-        dp = new boolean[m][n];
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                if(i==j){
-                    ch='r';
-                    dfs(matrix, i, j, 'r', m-i, n-j);
-                    ch=' ';
-                }
+        int m=matrix.length, n=matrix[0].length;
+        int top=0, bottom=m-1, left=0, right=n-1;
+        List<Integer> list = new ArrayList<>();
+        while(top<bottom && left<right){
+            for(int i=left; i<=right; i++){
+                list.add(matrix[top][i]);
+            }
+            top++;
+            for(int i=top; i<=bottom; i++){
+                list.add(matrix[i][right]);
+            }
+            right--;
+            for(int i=right; i>=left; i--){
+                list.add(matrix[bottom][i]);
+            }
+            bottom--;
+            for(int i=bottom; i>=top; i--){
+                list.add(matrix[i][left]);
+            }
+            left++;            
+        }
+        if(top==bottom){
+            for(int i=left; i<=right; i++){
+                list.add(matrix[top][i]);
+            }
+        }
+        if(left==right && top!= bottom){
+            for(int i=top; i<=bottom; i++){
+                list.add(matrix[i][left]);
             }
         }
         return list;
-    }
-    public void dfs(int[][] matrix, int i, int j, char dir, int m, int n){
-        if(i<matrix.length-m || i>m-1 || j<matrix[0].length-n || j>n-1 || dp[i][j]){
-            if(j>n-1){
-                ch='d';
-            }
-            else if(i>m-1){
-                ch='l';
-            }
-            else if(j<matrix[0].length-n){
-                ch='u';
-            }
-            else{
-                ch=' ';
-            }
-            return;
-        }
-        dp[i][j]=true;
-        list.add(matrix[i][j]);
-        if(ch=='r'){
-            dfs(matrix, i, j+1, 'r', m, n);
-        }
-        if(ch=='d'){
-            dfs(matrix, i+1, j, 'd', m, n);
-        }
-        if(ch=='l'){
-            dfs(matrix, i, j-1, 'l', m, n);
-        }
-        if(ch=='u'){
-            dfs(matrix, i-1, j, 'u', m, n);
-        }
     }
 }
