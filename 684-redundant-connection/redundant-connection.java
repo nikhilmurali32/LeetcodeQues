abd1d5169a8336866ed725cf7c86ea49750cf981
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-04-19 17:02:11 -0400

    Time: 2 ms (26.59%) | Memory: 45.4 MB (22.93%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,5 +1,4 @@
 class Solution {
-int[] res = new int[2];
 class DisjointSet{
     List<Integer> parent = new ArrayList<>();
     List<Integer> size = new ArrayList<>();
@@ -17,29 +16,33 @@ class DisjointSet{
         parent.set(node, par);
         return parent.get(node);
     }
-    public void joinBySize(int u, int v){
+    public int[] joinBySize(int u, int v){
         int ulp_u = findParent(u);
         int ulp_v = findParent(v);
         if(ulp_u==ulp_v){
-            res[0]=u;
-            res[1]=v;
-            return;
+            return new int[]{u, v};
         }
         else if(size.get(ulp_u)<size.get(ulp_v)){
             parent.set(ulp_u, ulp_v);
             size.set(ulp_v, size.get(ulp_u)+size.get(ulp_v));
+            return new int[2];
         }
         else{
             parent.set(ulp_v, ulp_u);
-            size.set(ulp_u, size.get(ulp_u)+size.get(ulp_v));            
+            size.set(ulp_u, size.get(ulp_u)+size.get(ulp_v)); 
+            return new int[2];           
         }
     }
 
 }
     public int[] findRedundantConnection(int[][] edges) {
         DisjointSet dis = new DisjointSet(edges.length);
+        int[] res = new int[2];
         for(int[] edge:edges){
-            dis.joinBySize(edge[0], edge[1]);
+            res = dis.joinBySize(edge[0], edge[1]);
+            if(res[0] != 0){
+                return res;
+            }
         }
         return res;
     }
