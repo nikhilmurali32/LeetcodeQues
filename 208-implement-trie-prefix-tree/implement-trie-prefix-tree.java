3ddd7aa926c57f0b777799d011549c76fb12d86d
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-04-19 14:08:27 -0400

    Time: 34 ms (53.91%) | Memory: 61.9 MB (66.39%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -1,29 +1,22 @@
 class Node{
-    Node links[] = new Node[26];
-    boolean flag=false;
-
-    boolean containsKey(char ch){
-        return (links[ch-'a']!=null);
+    Node[] links = new Node[26];
+    boolean flag = false;
+    boolean containsKey(char c){
+        return links[c-'a']!=null;
     }
-
-    void put(char ch, Node node){
-        links[ch-'a']=node;
+    Node get(char c){
+        return links[c-'a'];
     }
-
-    Node get(char ch){
-        return links[ch-'a'];
+    void put(Node n, char c){
+        links[c-'a'] = n;
     }
-
-    void setEnd(){
+    void setNode(){
         flag=true;
     }
-
-    boolean isEnd(){
-        return flag;
-    }
 }
+
 class Trie {
-    private static Node root;
+    Node root;
     public Trie() {
         root = new Node();
     }
@@ -31,34 +24,40 @@ class Trie {
     public void insert(String word) {
         Node node = root;
         for(int i=0; i<word.length(); i++){
-            if(!node.containsKey(word.charAt(i))){
-                node.put(word.charAt(i), new Node());
+            char ch=word.charAt(i);
+            if(!node.containsKey(ch)){
+                node.put(new Node(), ch);
             }
-           node = node.get(word.charAt(i));
+            node = node.get(ch);
         }
-        node.setEnd();
+        node.setNode();
     }
     
     public boolean search(String word) {
         Node node = root;
         for(int i=0; i<word.length(); i++){
-            if(!node.containsKey(word.charAt(i))){
+            char ch=word.charAt(i);
+            if(!node.containsKey(ch)){
                 return false;
             }
-            node = node.get(word.charAt(i));
+            node = node.get(ch);
         }
-        return node.isEnd();
+        if(node.flag==true){
+            return true;
+        } 
+        return false;       
     }
     
     public boolean startsWith(String prefix) {
         Node node = root;
         for(int i=0; i<prefix.length(); i++){
-            if(!node.containsKey(prefix.charAt(i))){
+            char ch=prefix.charAt(i);
+            if(!node.containsKey(ch)){
                 return false;
             }
-            node = node.get(prefix.charAt(i));
+            node = node.get(ch);
         }
-        return true;
+        return true;        
     }
 }
 
