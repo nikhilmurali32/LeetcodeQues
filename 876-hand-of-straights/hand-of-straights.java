0fc5e83bc36842ad4c1168f028bdaed720c8a5e5
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-04-25 09:59:38 -0400

    Time: 61 ms (37.93%) | Memory: 47.7 MB (81.52%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -4,31 +4,24 @@ class Solution {
         if(n%groupSize != 0){
             return false;
         }
-        int totalGroups = n/groupSize;
-        Arrays.sort(hand);
-        boolean[] visited = new boolean[n];
-
-        while(totalGroups>0){
-            int prev=-1;
-            int x=0;
-            for(int i=0; i<n && x<groupSize; i++){
-                if(visited[i]){
-                    continue;
-                }
-                if(i>0 && hand[i]==prev){
-                    continue;
-                }
-                visited[i]=true;
-                x++;
-                if(i!=0 && prev!=-1 && hand[i] != prev+1){
+        TreeMap<Integer, Integer> tmap = new TreeMap<>();
+        for(int h:hand){
+            tmap.put(h, tmap.getOrDefault(h,0)+1);
+        }
+        while(!tmap.isEmpty()){
+            int first = tmap.firstKey();
+            for(int i=first; i<first+groupSize; i++){
+                if(!tmap.containsKey(i)){
                     return false;
                 }
-                prev=hand[i];
+                int count=tmap.get(i);
+                if(count==1){
+                    tmap.remove(i);
+                }
+                else{
+                    tmap.put(i, count-1);
+                }
             }
-            if(x<groupSize){
-                return false;
-            }
-            totalGroups--;
         }
         return true;
     }
