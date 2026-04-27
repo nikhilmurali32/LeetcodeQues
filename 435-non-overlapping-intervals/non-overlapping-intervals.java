fcd6782e28798d34841d077ca60434c009d7271c
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-04-26 20:27:28 -0400

    Time: 47 ms (52.79%) | Memory: 115.6 MB (76.12%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -1,36 +1,15 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        Arrays.sort(intervals, (a,b)->(a[1]-b[1]==0?a[0]-b[0]:a[1]-b[1]));
-        int n = intervals.length;
-        // int[][] dp = new int[n+1][n+1];
-        // for(int[] arr:dp){
-        //     Arrays.fill(arr, -1);
-        // }
-        int count=0;
-        int prevEndTime = intervals[0][1];
-        for(int i=1; i<n; i++){
+        Arrays.sort(intervals, (a,b)->(a[0]-b[0]));
+        int prevEndTime = intervals[0][1], count=0;
+        for(int i=1; i<intervals.length; i++){
             if(intervals[i][0]<prevEndTime){
                 count++;
+                prevEndTime = Math.min(prevEndTime, intervals[i][1]);
+                continue;
             }
-            else{
-                prevEndTime=intervals[i][1];
-            }
+            prevEndTime = Math.max(prevEndTime, intervals[i][1]);
         }
         return count;
-        // return countMinElemtoRemove(intervals, 0, -1, dp);
     }
-    // public int countMinElemtoRemove(int[][] intervals, int currIndex, int prevIndex, int[][] dp){
-    //     if(currIndex >= intervals.length){
-    //         return dp[currIndex][prevIndex+1] = 0;
-    //     }
-    //     if(dp[currIndex][prevIndex+1] != -1){
-    //         return dp[currIndex][prevIndex+1];
-    //     }
-    //     int doNotTake = 1+countMinElemtoRemove(intervals, currIndex+1, prevIndex, dp);
-    //     int take = 100000;
-    //     if(prevIndex == -1 || intervals[currIndex][0] >= intervals[prevIndex][1]){
-    //         take = countMinElemtoRemove(intervals, currIndex+1, currIndex, dp);
-    //     }        
-    //     return dp[currIndex][prevIndex+1] = Math.min(take, doNotTake);
-    // }
 }
