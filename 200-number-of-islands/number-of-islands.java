228a7b51660e997210489dc4df95c4602aa78606
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-05-01 15:53:53 -0400

    Time: 4 ms (42.69%) | Memory: 51.4 MB (90.25%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,38 +1,41 @@
 class Solution {
     public int numIslands(char[][] grid) {
         int m=grid.length, n=grid[0].length;
-        Queue<int[]> landCells = new LinkedList<>();
         int count=0;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j] == '1'){
-                    grid[i][j]='0';
-                    landCells.add(new int[]{i,j});
-                    while(!landCells.isEmpty()){
-                        int size=landCells.size();
-                        for(int l=0; l<size; l++){
-                            int currI = landCells.peek()[0];
-                            int currJ = landCells.remove()[1];
-                            for(int k=-1; k<=1; k++){
-                                if(k==0){
-                                    continue;
-                                }
-                                if(currI+k >= 0 && currI+k<m && grid[currI+k][currJ]=='1'){
-                                    grid[currI+k][currJ]='0';
-                                    landCells.add(new int[]{currI+k, currJ});
-                                }
-                                if(currJ+k >= 0 && currJ+k<n && grid[currI][currJ+k]=='1'){
-                                    grid[currI][currJ+k]='0';
-                                    landCells.add(new int[]{currI, currJ+k});
-                                }
-                            }
-                        }
-                    }
+                    bfs(grid, i, j);
                     count++;
                 }
             }
         }
         return count;
 
+    }
+    public void bfs(char[][] grid, int i, int j){
+        int m=grid.length, n=grid[0].length;
+        grid[i][j]='0';
+        Queue<int[]> landCells = new LinkedList<>();
+        landCells.add(new int[]{i,j});
+        while(!landCells.isEmpty()){
+            int size=landCells.size();
+            int currI = landCells.peek()[0];
+            int currJ = landCells.remove()[1];
+            for(int k=-1; k<=1; k++){
+                if(k==0){
+                    continue;
+                }
+                if(currI+k >= 0 && currI+k<m && grid[currI+k][currJ]=='1'){
+                    grid[currI+k][currJ]='0';
+                    landCells.add(new int[]{currI+k, currJ});
+                }
+                if(currJ+k >= 0 && currJ+k<n && grid[currI][currJ+k]=='1'){
+                    grid[currI][currJ+k]='0';
+                    landCells.add(new int[]{currI, currJ+k});
+                }
+            }
+            
+        }
     }
 }
