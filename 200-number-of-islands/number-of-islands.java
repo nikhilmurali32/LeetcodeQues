930cc99ad5b3dccff699df9da36d0a634e639f9b
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-05-01 15:39:53 -0400

    Time: 6 ms (19.79%) | Memory: 51.5 MB (84.54%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,28 +1,38 @@
 class Solution {
-    int m;
-    int n;
     public int numIslands(char[][] grid) {
-        m=grid.length;
-        n=grid[0].length;
+        int m=grid.length, n=grid[0].length;
+        Queue<int[]> landCells = new LinkedList<>();
         int count=0;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(grid[i][j]=='1'){
+                if(grid[i][j] == '1'){
+                    grid[i][j]='0';
+                    landCells.add(new int[]{i,j});
+                    while(!landCells.isEmpty()){
+                        int size=landCells.size();
+                        for(int l=0; l<size; l++){
+                            int currI = landCells.peek()[0];
+                            int currJ = landCells.remove()[1];
+                            for(int k=-1; k<=1; k++){
+                                if(k==0){
+                                    continue;
+                                }
+                                if(currI+k >= 0 && currI+k<m && grid[currI+k][currJ]=='1'){
+                                    grid[currI+k][currJ]='0';
+                                    landCells.add(new int[]{currI+k, currJ});
+                                }
+                                if(currJ+k >= 0 && currJ+k<n && grid[currI][currJ+k]=='1'){
+                                    grid[currI][currJ+k]='0';
+                                    landCells.add(new int[]{currI, currJ+k});
+                                }
+                            }
+                        }
+                    }
                     count++;
-                    dfs(grid, i, j);
                 }
             }
         }
         return count;
-    }
-    public void dfs(char[][] grid, int i, int j){
-        if(i<0 || i>m-1 || j<0 || j>n-1 || grid[i][j]=='0'){
-            return;
-        }
-        grid[i][j]='0';
-        dfs(grid, i, j+1);
-        dfs(grid, i+1, j);
-        dfs(grid, i, j-1);
-        dfs(grid, i-1, j);
+
     }
 }
