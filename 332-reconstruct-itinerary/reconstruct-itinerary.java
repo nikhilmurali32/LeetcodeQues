e96ead5b6a5c5d7e69705d3cb2506f6e2759377e
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-05-04 17:50:08 -0400

    Time: 5 ms (99.85%) | Memory: 46.7 MB (96.42%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.java
+++ 332-reconstruct-itinerary/reconstruct-itinerary.java
@@ -1,25 +1,21 @@
 class Solution {
     public List<String> findItinerary(List<List<String>> tickets) {
         HashMap<String, PriorityQueue<String>> adjList = new HashMap<>();
-        List<String> ans = new LinkedList<>();
+        List<String> finalItenary = new LinkedList<>();
         for(List<String> ticket:tickets){
             if(!adjList.containsKey(ticket.get(0))){
-                PriorityQueue<String> pq = new PriorityQueue<>();
-                pq.add(ticket.get(1));
-                adjList.put(ticket.get(0), pq);
+                adjList.put(ticket.get(0), new PriorityQueue<>());
             }
-            else{
-                adjList.get(ticket.get(0)).add(ticket.get(1));
-            }
+            adjList.get(ticket.get(0)).add(ticket.get(1));
         }
-        dfs("JFK", adjList, ans);
-        return ans;
+        dfs("JFK", adjList, finalItenary);
+        return finalItenary;
     }
-    public void dfs(String city, HashMap<String, PriorityQueue<String>> adjList, List<String> list){
-        PriorityQueue<String> connectedCities = adjList.get(city);
-        while(connectedCities!= null && !connectedCities.isEmpty()){
-            dfs(connectedCities.remove(), adjList, list);
+    public void dfs(String src, HashMap<String, PriorityQueue<String>> adjList, List<String> finalItenary){
+        PriorityQueue<String> connectedCities = adjList.get(src);
+        while(connectedCities != null && !connectedCities.isEmpty()){
+            dfs(connectedCities.remove(), adjList, finalItenary);
         }
-        list.addFirst(city);
+        finalItenary.addFirst(src);
     }
 }
