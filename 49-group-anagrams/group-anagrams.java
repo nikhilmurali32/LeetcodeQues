9149214abfcf296f75c9a15cb33bee66ba487e22
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-06-04 18:19:29 -0400

    Time: 9 ms (32.47%) | Memory: 50.8 MB (10.63%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -2,11 +2,11 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String, List<String>> anagrams = new HashMap<>();
         for(String str:strs){
-            int[] freq = new int[26];
+            char[] freq = new char[101];
             for(char ch:str.toCharArray()){
                 freq[ch-'a']++;
             }
-            String sorted_str = Arrays.toString(freq);
+            String sorted_str = new String(freq);
             if(!anagrams.containsKey(sorted_str)){
                 anagrams.put(sorted_str, new ArrayList<>());
             }
