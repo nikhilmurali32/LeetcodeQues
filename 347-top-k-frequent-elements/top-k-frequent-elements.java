073f06ef6497cd6931cb60a635a36c0d2e55a5e7
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-06-04 18:51:32 -0400

    Time: 12 ms (84.26%) | Memory: 50.8 MB (6.41%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,23 +1,24 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int n=nums.length;
-        HashMap<Integer, Integer> freqMap = new HashMap<>();
+        HashMap<Integer, Integer> freq = new HashMap<>();
         for(int num:nums){
-            freqMap.put(num, freqMap.getOrDefault(num,0)+1);
+            freq.put(num, freq.getOrDefault(num,0)+1);
         }
-        List<List<Integer>> freqBucket = new ArrayList<>();
-        for(int i=0; i<=n; i++){
-            freqBucket.add(new ArrayList<>());
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i=0; i<=nums.length; i++){
+            list.add(new ArrayList<>());
         }
-        for(Map.Entry<Integer, Integer> entry:freqMap.entrySet()){
-            freqBucket.get(entry.getValue()).add(entry.getKey());
+        for(Map.Entry<Integer, Integer> entry:freq.entrySet()){
+            list.get(entry.getValue()).add(entry.getKey());
         }
         int[] res = new int[k];
         int ind=0;
-        for(int i=freqBucket.size()-1; i>=0 && ind<k; i--){
-            for(int elem:freqBucket.get(i)){
-                res[ind]=elem;
-                ind++;
+        for(int i=list.size()-1; i>=0 && ind<k; i--){
+            if(!list.get(i).isEmpty()){
+                for(int ele:list.get(i)){
+                    res[ind]=ele;
+                    ind++;
+                }
             }
         }
         return res;
