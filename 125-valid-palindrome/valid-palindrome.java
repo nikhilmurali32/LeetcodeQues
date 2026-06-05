5379a4156710a9ce373a7ae1669eb12c43720481
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-06-05 13:49:34 -0400

    Time: 4 ms (49.08%) | Memory: 46.7 MB (16.66%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -10,22 +10,34 @@ class Solution {
         for(char c='0'; c<='9'; c++){
             alpNum.add(c);
         }
-        StringBuilder sb = new StringBuilder();
-        for(char ch:s.toCharArray()){
-            if(alpNum.contains(ch)){
-                if(ch>='A' && ch<='Z'){
-                    ch = (char) (ch - 'A' + 'a');
-                }
-                sb.append(ch);
-            }
-        }
-        return checkPalindrome(sb.toString());
+        return checkPalindrome(s, alpNum);
     }
-    public boolean checkPalindrome(String str){
+    public boolean checkPalindrome(String str, HashSet<Character> alpNum){
         int l=0;
         int r=str.length()-1;
         while(l<r){
-            if(str.charAt(l) != str.charAt(r)){
+            while(l<str.length() && l<r && !alpNum.contains(str.charAt(l))){
+                l++;
+            }
+            if(l==str.length() || l>=r){
+                break;
+            }
+            char ch_l = str.charAt(l);
+            if(ch_l>='A' && ch_l<='Z'){
+                ch_l = (char) (ch_l - 'A' + 'a');
+            }
+
+            while(r>=0 && l<r && !alpNum.contains(str.charAt(r))){
+                r--;
+            }
+            if(r<0 || l>=r){
+                break;
+            }
+            char ch_r = str.charAt(r);
+            if(ch_r>='A' && ch_r<='Z'){
+                ch_r = (char) (ch_r - 'A' + 'a');
+            }        
+            if(ch_l != ch_r){
                 return false;
             }
             l++;
