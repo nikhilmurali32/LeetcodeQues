a1d9fa88112e2b7b03b01ecc2cb6fa649e7d679e
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-06-05 01:24:27 -0400

    Time: 12 ms (21.62%) | Memory: 46.4 MB (56.89%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,31 +1,13 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashMap<String, HashSet<Character>> hmap = new HashMap<>();
+        HashSet<String> hset = new HashSet<>();
         int m=board.length, n=board[0].length;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(board[i][j]=='.'){
                     continue;
                 }
-                String rowKey = "R"+String.valueOf(i);
-                String colKey = "C"+String.valueOf(j);
-                String boxKey = String.valueOf(i/3)+"B"+String.valueOf(j/3);
-                if(!hmap.containsKey(rowKey)){
-                    hmap.put(rowKey, new HashSet<>());
-                }
-                if(!hmap.get(rowKey).add(board[i][j])){
-                    return false;
-                }
-                if(!hmap.containsKey(colKey)){
-                    hmap.put(colKey, new HashSet<>());
-                }
-                if(!hmap.get(colKey).add(board[i][j])){
-                    return false;
-                }
-                if(!hmap.containsKey(boxKey)){
-                    hmap.put(boxKey, new HashSet<>());
-                }
-                if(!hmap.get(boxKey).add(board[i][j])){
+                if(!hset.add("R"+i+board[i][j]) || !hset.add("C"+j+board[i][j]) || !hset.add(i/3 + "B" + j/3+board[i][j])){
                     return false;
                 }
             }
