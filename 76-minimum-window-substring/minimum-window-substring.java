a8f7098b93bc621cedbd416846614144d61e02c7
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-06-06 13:55:31 -0400

    Time: 114 ms (12.59%) | Memory: 48.2 MB (5.31%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,40 +1,39 @@
 class Solution {
     public String minWindow(String s, String t) {
-        HashMap<Character, Integer> hmap_t = new HashMap<>();
+        HashMap<Character, Integer> uniqueS = new HashMap<>();
+        HashSet<Character> hset = new HashSet<>();
         for(char ch:t.toCharArray()){
-            hmap_t.put(ch, hmap_t.getOrDefault(ch,0)+1);
+            uniqueS.put(ch, uniqueS.getOrDefault(ch, 0)+1);
+            hset.add(ch);
         }
-        int count = hmap_t.size();
-        int i=0, j=0;
-        int min=Integer.MAX_VALUE;
-        int startInd=0;
-        while(j<s.length()){
-            char ch=s.charAt(j);
-            if(hmap_t.containsKey(ch)){
-                hmap_t.put(ch, hmap_t.get(ch)-1);
-                if(hmap_t.get(ch)==0){
+        int count=hset.size();
+        int i=0, j=0, n=s.length();
+        String res= "";
+        int minLen = n+1;
+        while(i<=j && j<n){
+            char ch = s.charAt(j);
+            if(uniqueS.containsKey(ch)){
+                uniqueS.put(ch, uniqueS.getOrDefault(ch, 0) - 1);
+                if(uniqueS.get(ch)==0){
                     count--;
+                }
+                while(count==0){
+                    if((j-i+1)<minLen){
+                        minLen = j-i+1;
+                        res = s.substring(i, j+1);
+                    }
+                    char leftChar = s.charAt(i);
+                    if(uniqueS.containsKey(leftChar)){
+                        uniqueS.put(leftChar, uniqueS.get(leftChar) + 1);
+                        if(uniqueS.get(leftChar)>0){
+                            count++;
+                        }
+                    }
+                    i++;                    
                 }
             }
             j++;
-            while(count==0){
-                if((j-i)<min){
-                    min=j-i;
-                    startInd=i;
-                }
-                char start=s.charAt(i);
-                if(hmap_t.containsKey(start)){
-                    hmap_t.put(start, hmap_t.get(start)+1);
-                    if(hmap_t.get(start)>0){
-                        count++;
-                    }
-                }
-                i++;
-            }
         }
-        if(min==Integer.MAX_VALUE){
-            return "";
-        }
-        return s.substring(startInd, startInd+min);
+        return res;
     }
 }
