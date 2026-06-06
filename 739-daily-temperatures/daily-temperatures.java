c417ddbdc58a1b310c68154d83f18152f4906f20
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-06-06 15:49:25 -0400

    Time: 26 ms (81.53%) | Memory: 102 MB (95.47%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,14 +1,17 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        Deque<Integer> temp = new ArrayDeque<>();
-        for(int i=0; i<temperatures.length; i++){
-            while(!temp.isEmpty() && temperatures[i]>temperatures[temp.peekLast()]){
-                temperatures[temp.peekLast()] = i-temp.removeLast();
+        Deque<Integer> stack = new ArrayDeque<>();
+        int n=temperatures.length;
+        int i=0;
+        while(i<n){
+            while(!stack.isEmpty() && temperatures[i]>temperatures[stack.peekLast()]){
+                temperatures[stack.peekLast()] = i-stack.removeLast();
             }
-            temp.addLast(i);
+            stack.addLast(i);
+            i++;
         }
-        while(!temp.isEmpty()){
-            temperatures[temp.remove()]=0;
+        while(!stack.isEmpty()){
+            temperatures[stack.removeLast()]=0;
         }
         return temperatures;
     }
