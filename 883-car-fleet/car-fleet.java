0cd212f3c92b3344c6682f24f315054ac893885f
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-06-07 11:42:09 -0400

    Time: 844 ms (5.11%) | Memory: 96.9 MB (64.26%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -1,20 +1,23 @@
 class Solution {
+    // time to reach = [1, 1, 12, 7, 6]
+    // sort TtoReach = [[10,1], [8,1], [5,7], [3,6], [0, 12]]
     public int carFleet(int target, int[] position, int[] speed) {
         int n=position.length;
-        double[][] posTime = new double[n][2];
+        double[][] time = new double[n][2];
         for(int i=0; i<n; i++){
-            posTime[i][0] = position[i];
-            posTime[i][1] = (double)(target-position[i])/speed[i];
+            time[i][0]=target-position[i];
+            time[i][1]=((double)(target-position[i])/speed[i]);
+            System.out.println(time[i][1]);
         }
-        Arrays.sort(posTime, (a,b) -> Double.compare(a[0], b[0]));
-        int count=0;
-        double currTime=0;
-        for(int i=n-1; i>=0; i--){
-            if(posTime[i][1]>currTime){
-                currTime=posTime[i][1];
-                count++;
+        Arrays.sort(time, (a,b) -> Double.compare(a[0], b[0]));
+        int i=0;
+        Deque<Double> stack = new ArrayDeque<>();
+        while(i<n){
+            if(stack.isEmpty() || stack.peekLast()<time[i][1]){
+                stack.addLast(time[i][1]);
             }
+            i++;
         }
-        return count;
+        return stack.size();
     }
 }
