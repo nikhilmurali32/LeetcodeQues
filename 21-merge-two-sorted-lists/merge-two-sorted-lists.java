345cd944c37ea279fdc47314206798900a45868d
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-06-08 19:32:32 -0400

    Time: 0 ms (100.00%) | Memory: 44.1 MB (86.41%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -23,15 +23,11 @@ class Solution {
             }
             curr = curr.next;
         }
-        while(list1!=null){
-            curr.next = new ListNode(list1.val);
-            curr = curr.next;
-            list1 = list1.next;
+        if(list1!=null){
+            curr.next = list1;
         }
-        while(list2!=null){
-            curr.next = new ListNode(list2.val);
-            curr = curr.next;
-            list2 = list2.next;
+        if(list2!=null){
+            curr.next = list2;
         }
         return newHead.next;
     }
