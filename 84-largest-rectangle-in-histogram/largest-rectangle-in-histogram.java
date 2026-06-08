5efd9bd5b1b4e76a6d93a4e8384682c30fd448f0
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-06-08 12:00:34 -0400

    Time: 33 ms (85.74%) | Memory: 78.8 MB (22.49%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,34 +1,27 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
         int n=heights.length;
-        Stack<Integer> st_left = new Stack<>();
-        int[] left_max = new int[n];
-        left_max[0]=1;
-        st_left.push(0);
-
-        Stack<Integer> st_right = new Stack<>();
-        int[] right_max = new int[n];
-        right_max[n-1]=1;
-        st_right.push(n-1);
-
-        for(int i=1; i<n; i++){
-            while(!st_left.isEmpty() && heights[i]<=heights[st_left.peek()]){
-                st_left.pop();
-            }
-            left_max[i] = st_left.isEmpty()?i+1:i-st_left.peek();
-            st_left.push(i);
-
-            while(!st_right.isEmpty() && heights[n-i-1]<=heights[st_right.peek()]){
-                st_right.pop();
-            }
-            right_max[n-i-1] = st_right.isEmpty()?i+1:st_right.peek()-(n-i-1);
-            st_right.push(n-i-1);
-        }
-
-        int max_area = 0;
+        int[] leftMax = new int[n];
+        Deque<Integer> stackLeft = new ArrayDeque<>();
+        int[] rightMax = new int[n];
+        Deque<Integer> stackRight = new ArrayDeque<>();
         for(int i=0; i<n; i++){
-            max_area = Math.max(max_area, heights[i]*(left_max[i]+right_max[i]-1));
+            while(!stackLeft.isEmpty() && heights[i]<=heights[stackLeft.peekLast()]){
+                stackLeft.removeLast();
+            }
+            leftMax[i] = stackLeft.isEmpty()?i+1:i-stackLeft.peekLast();
+            stackLeft.addLast(i);
+
+            while(!stackRight.isEmpty() && heights[n-i-1]<=heights[stackRight.peekLast()]){
+                stackRight.removeLast();
+            }
+            rightMax[n-i-1] = stackRight.isEmpty()?n-(n-i)+1:stackRight.peekLast()-(n-i-1);
+            stackRight.addLast(n-i-1);
         }
-        return max_area;
+        int maxHeight=0;
+        for(int i=0; i<n; i++){
+            maxHeight = Math.max(maxHeight, (leftMax[i]+rightMax[i]-1)*heights[i]);
+        }
+        return maxHeight;
     }
 }
