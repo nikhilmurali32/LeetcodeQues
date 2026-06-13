c488723c73c651d244a79f5535dfe3681119d27f
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-06-13 13:11:00 -0400

    Time: 30 ms (31.61%) | Memory: 48.5 MB (11.84%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,31 +1,47 @@
 class Solution {
+    class Task{
+        char ch;
+        int rem;
+        int time;
+        Task(char ch, int rem){
+            this.ch=ch;
+            this.rem=rem;
+        }
+    }
     public int leastInterval(char[] tasks, int n) {
         int[] freq = new int[26];
         for(char task:tasks){
             freq[task-'A']++;
         }
-        PriorityQueue<Integer> maxFreq = new PriorityQueue<>((a,b) -> (b-a));
-        for(int task: freq){
-            if(task>0){
-                maxFreq.add(task);
+        PriorityQueue<Task> maxFreq = new PriorityQueue<>((a,b)->Integer.compare(b.rem,a.rem));
+        for(int i=0; i<26; i++){
+            if(freq[i]>0){
+                maxFreq.add(new Task((char)('A'+i), freq[i]));
             }
         }
-        Queue<int[]> waiting = new LinkedList<>();
-        int timer=0;
-        while(!maxFreq.isEmpty() || !waiting.isEmpty()){
-            if(!maxFreq.isEmpty()){
-                int currTask = maxFreq.poll();
-                currTask--;
-                if(currTask>0){
-                    waiting.add(new int[]{currTask, timer+n});
-                }
+        int time=0;
+        Queue<Task> waitingTasks = new LinkedList<>();
+        int completedTasks=0;
+        int totalTasks=maxFreq.size();
+        while(completedTasks < totalTasks){
+            while(!waitingTasks.isEmpty() && waitingTasks.peek().time<time){
+                maxFreq.add(waitingTasks.remove());
             }
-            if(!waiting.isEmpty() && waiting.peek()[1]==timer){
-                int[] freeTask = waiting.remove();
-                maxFreq.add(freeTask[0]);
+            if(maxFreq.isEmpty()){
+                time++;
+                continue;
             }
-            timer++;
+            Task currTask = maxFreq.remove();
+            currTask.rem--;
+            currTask.time = time+n;
+            if(currTask.rem==0){
+                completedTasks++;
+            }
+            else{
+                waitingTasks.add(currTask);
+            }
+            time++;
         }
-        return timer;
+        return time;
     }
 }
