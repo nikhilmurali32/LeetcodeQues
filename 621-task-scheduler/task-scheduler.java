0438aa68965c8b545e70ddcffc6c77b742018077
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-06-13 14:17:51 -0400

    Time: 4 ms (77.86%) | Memory: 48.5 MB (22.40%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,48 +1,22 @@
 class Solution {
-    class Task{
-        char ch;
-        int rem;
-        int time;
-        Task(char ch, int rem){
-            this.ch=ch;
-            this.rem=rem;
-        }
-    }
     public int leastInterval(char[] tasks, int n) {
         int[] freq = new int[26];
+        int max=0, maxCount=0;
         for(char task:tasks){
             freq[task-'A']++;
+            if(freq[task-'A']==max){
+                maxCount++;
+            }
+            else if(freq[task-'A']>max){
+                max=freq[task-'A'];
+                maxCount=1;
+            }   
         }
-        PriorityQueue<Task> maxFreq = new PriorityQueue<>((a,b)->Integer.compare(b.rem,a.rem));
-        for(int i=0; i<26; i++){
-            if(freq[i]>0){
-                maxFreq.add(new Task((char)('A'+i), freq[i]));
-            }
-        }
-        int time=0;
-        Queue<Task> waitingTasks = new LinkedList<>();
-        int completedTasks=0;
-        int totalTasks=maxFreq.size();
-        while(!maxFreq.isEmpty() || completedTasks < totalTasks){
-            while(!waitingTasks.isEmpty() && waitingTasks.peek().time<=time){
-                maxFreq.add(waitingTasks.remove());
-            }
-            if(maxFreq.isEmpty()){
-                int currWaitTime = waitingTasks.peek().time;
-                time = currWaitTime;
-                continue;
-            }
-            Task currTask = maxFreq.remove();
-            currTask.rem--;
-            currTask.time = time+n+1;
-            if(currTask.rem==0){
-                completedTasks++;
-            }
-            else{
-                waitingTasks.add(currTask);
-            }
-            time++;
-        }
-        return time;
+        int partCount = max-1;
+        int partLength = n-(maxCount-1);
+        int emptySlots = partCount*partLength;
+        int availableTasks = tasks.length-max*maxCount;
+        int idles = Math.max(0, emptySlots-availableTasks);
+        return tasks.length+idles;
     }
 }
