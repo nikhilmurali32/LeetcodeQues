d97e6f85a741bc48edf96a18d0e196b682de9e71
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-06-13 11:47:42 -0400

    Time: 57 ms (64.02%) | Memory: 77.6 MB (5.26%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,49 +1,13 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         int n=nums.length;
-        if(n==1 && k==n){
-            return nums[0];
-        }
-        return quickSelect(nums, 0, n-1, nums.length-k);
-    }
-    public int quickSelect(int[] nums, int l, int r, int k){
-        if(l<=r){
-            int pivot = findPivotIndex(nums, l, r, k);
-            if(pivot==k){
-                return nums[pivot];
-            }
-            else if(pivot<k){
-                return quickSelect(nums, pivot+1, r, k);
-            }
-            else{
-                return quickSelect(nums, l, pivot-1, k);
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b)->Integer.compare(b,a));
+        for(int num:nums){
+            maxHeap.add(num);
+            if(maxHeap.size()>n-k+1){
+                maxHeap.remove();
             }
         }
-        return -1;
-    }
-    public int findPivotIndex(int[] nums, int l, int r, int k){
-        int pivot = l + (r-l)/2;
-        int ind=l;
-        swap(nums, l, pivot);
-        while(l<r){
-            while(l<=r && nums[l]<=nums[ind]){
-                l++;
-            }
-            while(l<=r && nums[r]>nums[ind]){
-                r--;
-            }
-            if(l<r){
-                swap(nums, l, r);
-            }
-        }
-        if(r>=0){
-            swap(nums, r, ind);
-        }
-        return r;
-    }
-    public void swap(int[] nums, int l, int r){
-        int temp = nums[r];
-        nums[r]=nums[l];
-        nums[l]=temp;
+        return maxHeap.peek();
     }
 }
