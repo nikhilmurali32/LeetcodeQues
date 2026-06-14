c746806689f3516d397c7d35c6dbf08ead55284c
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-06-14 19:45:48 -0400

    Time: 6 ms (30.90%) | Memory: 44.6 MB (87.27%) - LeetSync

--- 71-simplify-path/simplify-path.java
+++ 71-simplify-path/simplify-path.java
@@ -1,47 +1,21 @@
 class Solution {
     public String simplifyPath(String path) {
-        StringBuilder sb = new StringBuilder();
-        for(char ch:path.toCharArray()){
-            if(sb.isEmpty()){
-                sb.append('/');
-                continue;
-            }
-            else if(ch=='/'){
-                if(sb.charAt(sb.length()-1)=='/'){
-                    continue;
-                }
-            }
-            sb.append(ch);
-        }
-        System.out.println(sb.toString());
-        String newString = sb.toString();
-        String[] strArr = newString.split("/");
-        List<String> list = new ArrayList<>();
+        String[] strArr = path.split("/");
+        Deque<String> dir = new ArrayDeque<>();
         for(String str:strArr){
-            if(str.equals(".")){
+            if(str.isEmpty() || str.equals(".")){
                 continue;
             }
             else if(str.equals("..")){
-                if(list.size()==0){
-                    continue;
+                if(!dir.isEmpty()){
+                    dir.removeLast();
                 }
-                list.remove(list.size()-1);
             }
             else{
-                list.add(str);
+                dir.addLast(str);
             }
         }
-        if(list.size()==0){
-            return "/";
-        }
-        sb.setLength(0);
-        for(int i=0; i<list.size(); i++){
-            sb.append("/").append(list.get(i));
-        }
-        sb.deleteCharAt(0);
-        if(sb.isEmpty() || sb.charAt(0) != '/'){
-            sb.insert(0, '/');
-        }
-        return sb.toString();
+        String newStr = String.join("/", dir);
+        return "/"+newStr;
     }
 }
