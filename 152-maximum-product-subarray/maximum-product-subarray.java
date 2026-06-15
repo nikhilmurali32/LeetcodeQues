e56a4f5e7e51f24c5520761875455ab5e0ac62ba
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-06-15 12:50:49 -0400

    Time: 1 ms (92.36%) | Memory: 47 MB (86.65%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,21 +1,3 @@
-// class Solution {
-//     public int maxProduct(int[] nums) {
-//         int negProd = 1;
-//         int currProd = 1;
-//         int maxProd = Integer.MIN_VALUE;
-//         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-//         for(int num:nums){
-//             currProd *= num;
-//             if(currProd<0){
-//                 if(!minHeap.isEmpty()){
-//                     maxProd = Math.max(maxProd, currProd/minHeap.peek());
-//                 }
-//                 minHeap.add(currProd);
-//             }
-//         }
-//         return maxProd;
-//     }
-// }
 class Solution {
     public int maxProduct(int[] nums) {
         int negProd = 1;
