ef3dda06444e6e35ed57edcaf3bf5461a7b1b5aa
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-06-15 23:04:37 -0400

    Time: 7 ms (46.84%) | Memory: 43.7 MB (46.93%) - LeetSync

--- 768-partition-labels/partition-labels.java
+++ 768-partition-labels/partition-labels.java
@@ -1,22 +1,27 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        int[] rightmostIndex = new int[26];
-        int n=s.length();
-        for(int i=0; i<n; i++){
-            rightmostIndex[s.charAt(i)-'a'] = i;
+        int[] freq = new int[26];
+        for(char ch:s.toCharArray()){
+            freq[ch-'a']++;
         }
-        int maxRight = 0;
-        StringBuilder sb = new StringBuilder();
+        int i=0, j=0;
+        int count=0;
         List<Integer> list = new ArrayList<>();
-        for(int i=0; i<n; i++){
-            char ch = s.charAt(i);
-            maxRight = Math.max(maxRight, rightmostIndex[ch-'a']);
-            sb.append(ch);
-            if(i==maxRight){
-                list.add(sb.length());
-                sb.setLength(0);
-                maxRight=0;
+        HashSet<Character> hset = new HashSet<>();
+        while(i<=j && j<s.length()){
+            freq[s.charAt(j)-'a']--;
+            if(hset.add(s.charAt(j))){
+                count += freq[s.charAt(j)-'a'];
             }
+            else{
+                count--;
+            }
+            if(count==0){
+                list.add(j-i+1);
+                i=j+1;
+                hset.clear();
+            }
+            j++;
         }
         return list;
     }
