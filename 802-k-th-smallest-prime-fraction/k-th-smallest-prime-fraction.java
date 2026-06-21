f70beaf6e401dc88d3ca70b94dea44a5e7169b9c
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-06-21 17:37:05 -0400

    Time: 291 ms (45.91%) | Memory: 47.3 MB (49.64%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
@@ -8,22 +8,18 @@ class Solution {
         }
     }
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
-        PriorityQueue<Element> pq = new PriorityQueue<>((a,b)->Double.compare((double)arr[a.i]/arr[a.j],(double)arr[b.i]/arr[b.j]));
+        PriorityQueue<Element> pq = new PriorityQueue<>((a,b)->Double.compare((double)arr[a.i]*arr[b.j],(double)arr[b.i]*arr[a.j]));
 
         Arrays.sort(arr);
         for (int j = 1; j < arr.length; j++) {
             pq.add(new Element(0, j));
         }
-        int[] ans= new int[2];
-        while(k>0){
+        while(--k>0){
             Element element = pq.remove();
-            ans[0]=arr[element.i];
-            ans[1]=arr[element.j];
-            k--;
             if(element.i+1<element.j){
                 pq.add(new Element(element.i+1, element.j));
             }
         }
-        return ans;        
+        return new int[]{arr[pq.peek().i], arr[pq.peek().j]};        
     }
 }
