8da95c81ebed4e2b7c02c2f9f48e7e5c4309b0e5
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-06-29 13:23:15 -0400

    Time: 4 ms (16.60%) | Memory: 47.1 MB (73.77%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,57 +1,54 @@
-// int count=0;
-class disjointSet{
-    List<Integer> parent = new ArrayList<>();
-    List<Integer> size = new ArrayList<>();
-    int components;
-    disjointSet(int n){
-        // components=n;
-        for(int i=0; i<n; i++){
-            parent.add(i);
-            size.add(1);
-        }
-    }
-    public int ulp(int node){
-        if(parent.get(node)==node){
-            return node;
-        }
-        int par = ulp(parent.get(node));
-        parent.set(node, par);
-        return parent.get(node);
-    }
-    public void joinBySize(int u, int v){
-        int ulp_u=ulp(u);
-        int ulp_v=ulp(v);
-        if(ulp_u==ulp_v){
-            return;
-        }
-        else if(size.get(ulp_u)<size.get(ulp_v)){
-            parent.set(ulp_u, ulp_v);
-            size.set(ulp_v, size.get(ulp_v)+size.get(ulp_u));
-        }
-        else{
-            parent.set(ulp_v, ulp_u);
-            size.set(ulp_u, size.get(ulp_u)+size.get(ulp_v));
-        }
-        // components--;
-    }
-}
 class Solution {
+    class DisjointSet{
+        List<Integer> parent = new ArrayList<>();
+        List<Integer> size = new ArrayList<>();
+        DisjointSet(int n){
+            for(int i=0; i<n; i++){
+                parent.add(i);
+                size.add(1);
+            }
+        }
+        public int ultimateP(int node){
+            if(parent.get(node)==node){
+                return node;
+            }
+            int par = ultimateP(parent.get(node));
+            parent.set(node, par);
+            return parent.get(node);
+        }
+        public void connect(int u, int v){
+            int ulP_u = ultimateP(u);
+            int ulP_v = ultimateP(v);
+            if(ulP_u==ulP_v){
+                return;
+            }
+            else if(size.get(ulP_u)>size.get(ulP_v)){
+                parent.set(ulP_v, ulP_u);
+                size.set(ulP_u, ulP_u+ulP_v);
+            }
+            else{
+                parent.set(ulP_u, ulP_v);
+                size.set(ulP_v, ulP_u+ulP_v);                
+            }
+        }
+    }
     public int findCircleNum(int[][] isConnected) {
-        disjointSet dis = new disjointSet(isConnected.length);
-        // int count=0;
-        for(int i=0; i<isConnected.length; i++){
-            for(int j=0; j<isConnected[i].length; j++){
+        DisjointSet dis = new DisjointSet(isConnected.length);
+        int n=isConnected.length;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
                 if(i==j){
                     continue;
                 }
-                else if(isConnected[i][j]==1){
-                    dis.joinBySize(i, j);
+                if(isConnected[i][j]==1){
+                    dis.connect(i, j);
                 }
             }
         }
+        List<Integer> p = dis.parent;
         int count=0;
-        for(int i=0; i<isConnected.length; i++){
-            if(dis.parent.get(i)==i){
+        for(int i=0; i<p.size(); i++){
+            if(i==p.get(i)){
                 count++;
             }
         }
