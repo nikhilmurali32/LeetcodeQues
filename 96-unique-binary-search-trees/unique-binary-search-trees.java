75be675a483ed56c6417969e211d6f2e3cb28927
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-06-28 22:48:46 -0400

    Time: 0 ms (100.00%) | Memory: 42.4 MB (12.60%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,14 +1,23 @@
 class Solution {
     public int numTrees(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        helper(n, dp);
+        return dp[n];
+    }
+    public int helper(int n, int[] dp){
         if(n==0 || n==1){
-            return 1;
+            return dp[n]=1;
+        }
+        if(dp[n] != -1){
+            return dp[n];
         }
         int totalCount=0;
         for(int i=1; i<n+1; i++){
-            int countLeft = numTrees(i-1);
-            int countRight = numTrees(n-i);
+            int countLeft = helper(i-1, dp);
+            int countRight = helper(n-i, dp);
             totalCount += countLeft*countRight;
         }
-        return totalCount;
+        return dp[n]=totalCount;
     }
 }
