49444a14940666e704bf7c490ef8f744513b2c22
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-08-09 11:09:53 -0400

    Time: 6 ms (85.44%) | Memory: 46.6 MB (39.47%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,28 +1,39 @@
 class Solution {
+    class Node{
+        int city;
+        int dist;
+        int stops;
+        Node(int city, int dist, int stops){
+            this.city=city;
+            this.dist=dist;
+            this.stops = stops;
+        }
+    }
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        List<List<int[]>> adjList = new ArrayList<>();
-        for(int i=0; i<=n; i++){
+        List<List<Node>> adjList = new ArrayList<>();
+        for(int i=0; i<n; i++){
             adjList.add(new ArrayList<>());
         }
         for(int[] flight:flights){
-            adjList.get(flight[0]).add(new int[]{flight[1], flight[2]});
+            adjList.get(flight[0]).add(new Node(flight[1], flight[2], 0));
         }
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{src, 0, 0});
-        int[] dist = new int[n];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        while(!q.isEmpty()){
-            int[] curr=q.poll();
-            int from=curr[0];
-            int cost=curr[1];
-            int stops = curr[2];
-            for(int[] nei:adjList.get(from)){
-                if(cost+nei[1] < dist[nei[0]] && stops<=k){
-                    dist[nei[0]]=cost+nei[1];
-                    q.add(new int[]{nei[0], cost+nei[1], stops+1});
-                }
+        PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> Integer.compare(a.dist, b.dist));
+        pq.add(new Node(src, 0, 0));
+        int[] minStops = new int[n];
+        Arrays.fill(minStops, Integer.MAX_VALUE);
+        while(!pq.isEmpty()){
+            Node node = pq.remove();
+            if(node.city==dst){
+                return node.dist;
+            }
+            if(node.stops>k || minStops[node.city]<=node.stops){
+                continue;
+            }
+            minStops[node.city] = node.stops;
+            for(Node nei:adjList.get(node.city)){
+                pq.add(new Node(nei.city, node.dist+nei.dist, node.stops+1));
             }
         }
-        return dist[dst]==Integer.MAX_VALUE?-1:dist[dst];
+        return -1;
     }
 }
