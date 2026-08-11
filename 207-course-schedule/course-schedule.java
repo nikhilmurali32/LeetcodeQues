082afac5bc8392eb33426d929dd432589ef30d2f
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-08-11 19:00:32 -0400

    Time: 7 ms (56.12%) | Memory: 47.3 MB (33.71%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -4,28 +4,29 @@ class Solution {
         for(int i=0; i<numCourses; i++){
             adjList.add(new ArrayList<>());
         }
-        int[] indegrees = new int[numCourses];
+        int[] inDegrees = new int[numCourses];
         for(int[] pre:prerequisites){
             adjList.get(pre[1]).add(pre[0]);
-            indegrees[pre[0]]++;
+            inDegrees[pre[0]]++;
         }
         Queue<Integer> q = new LinkedList<>();
-        for(int i=0; i<indegrees.length; i++){
-            if(indegrees[i]==0){
+        for(int i=0; i<numCourses; i++){
+            if(inDegrees[i]==0){
                 q.add(i);
             }
         }
-        int tot=0;
+        int count=0;
         while(!q.isEmpty()){
             int curr = q.remove();
-            tot++;
-            for(int course:adjList.get(curr)){
-                indegrees[course]--;
-                if(indegrees[course]==0){
-                    q.add(course);
+            for(int nei:adjList.get(curr)){
+                inDegrees[nei]--;
+                if(inDegrees[nei]==0){
+                    q.add(nei);
                 }
             }
+            count++;
         }
-        return tot==numCourses;
+        return numCourses==count;
+
     }
 }
