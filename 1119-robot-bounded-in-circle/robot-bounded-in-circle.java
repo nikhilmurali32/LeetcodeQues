35cf0c4c08e962be09fb5ce7010b190a21f56213
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-08-23 18:11:04 -0400

    Time: 0 ms (100.00%) | Memory: 43 MB (20.92%) - LeetSync

---
+++ 1119-robot-bounded-in-circle/robot-bounded-in-circle.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isRobotBounded(String instructions) {
+        int x=0,y=0, i=0;
+        int[][] dirs = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        for(char ch:instructions.toCharArray()){
+            if(ch=='R'){
+                i = (i+1)%4;
+            }
+            else if(ch=='L'){
+                i = (i+3)%4;
+            }
+            else{
+                x += dirs[i][0];
+                y += dirs[i][1];
+            }
+        }
+        return x==0 && y==0 || i>0;
+    }
+}
