3aebebac24635f46fa25e9b50b9e55f77d130065
Nikhil Murali <67653205+nikhilmurali32@users.noreply.github.com>
2026-08-23 14:17:20 -0400

    Time: 1 ms (99.94%) | Memory: 45.5 MB (33.34%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,56 +1,20 @@
 class Solution {
     public int compress(char[] chars) {
-        if(chars.length==1){
-            return 1;
-        }
-        int i=0, j=1, ind=0;
-        while(i<j && j<chars.length){
-            if(chars[j]==chars[i]){
-                j++;
+        int ind=0, indAns=0;
+        while(ind < chars.length){
+            char curr = chars[ind];
+            int count=0;
+            while(ind<chars.length && chars[ind]==curr){
+                ind++;
+                count++;
             }
-            else{
-                chars[ind]=chars[i];
-                ind++;
-                System.out.println(ind);
-                if(j-i==1){
-                    i=j;
-                    j++;
-                    continue;
+            chars[indAns++] = curr;
+            if(count != 1){
+                for(char ch:Integer.toString(count).toCharArray()){
+                    chars[indAns++] = ch;
                 }
-                else if((j-i)<10){
-                    chars[ind]=(char)(j-i+'0');
-                }
-                else{                    
-                    String str = String.valueOf(j-i);
-                    for(char ch: str.toCharArray()){
-                        chars[ind]=ch;
-                        ind++;
-                    }
-                    i=j;
-                    j++;
-                    continue;
-                }
-                ind++;
-                i=j;
-                j++;
             }
         }
-        chars[ind]=chars[i];
-        ind++;
-        if(j-i==1){
-            return ind;
-        }
-        else if((j-i)<10){
-            chars[ind]=(char)(j-i+'0');
-        }
-        else{                   
-            String str = String.valueOf(j-i);
-            for(char ch: str.toCharArray()){
-                chars[ind]=ch;
-                ind++;
-            }
-            return ind;
-        }
-        return ind+1;
+        return indAns;
     }
 }
